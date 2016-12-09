f52687b939dd23c78b20aa743f8aaab8d90182e6
Hsianghung2015 <hhhung@physics.utexas.edu>
2016-12-09 13:41:52 -0500

    cracking code

---
+++ Crackingcode_interview/4.trees/Q4.4_linkedLists_layer.py
@@ -0,0 +1,73 @@
+## Q4.4 generate linked lists for each layer
+##
+class treeNode():
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+        
+class Node():
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+        self.prev = None
+
+def find_depth(tree, heads, linkList, depth):
+    if tree != None:
+        print(tree.val, depth)
+        if depth not in heads:
+            linkList[depth] = Node(tree.val)
+            heads[depth] = linkList[depth]
+        else:
+            #print (heads[depth].val)
+            linkList[depth].next = Node(tree.val)
+            linkList[depth] = linkList[depth].next
+            
+        find_depth(tree.left, heads, linkList, depth+1)
+        find_depth(tree.right, heads, linkList, depth+1)
+
+
+
+## ----------------
+## test function
+
+## creating a  tree:
+A = treeNode(0)
+B = treeNode(1)
+C = treeNode(2)
+D = treeNode(3)
+E = treeNode(4)
+F = treeNode(5)
+G = treeNode(6)
+A.left = B
+A.right = C
+B.left = D
+B.right = E
+C.left = F
+C.right = G
+
+H = treeNode(7)
+I = treeNode(8)
+J = treeNode(9)
+K = treeNode(10)
+E.left = H
+E.right = I
+F.right = J
+G.left = K
+
+
+heads = {}
+linkList ={}
+find_depth(A, heads, linkList, 0)
+
+
+print ('linked lists:')
+#print (heads)
+
+for depth in heads:
+    node = heads[depth]
+    print (node.val)
+    while node.next != None:
+        node = node.next
+        print (node.val)
+    print ('------')
--- Crackingcode_interview/9.dynamic_programming/QB_shrink_aNumber.py
+++ Crackingcode_interview/9.dynamic_programming/QB_shrink_aNumber.py
@@ -1,4 +1,4 @@
-## Q. bonus questions: shrink a number
+## Q. bonus questions: minimum steps to shrink n -> 1
 ## On a positive integer, you can perform any one of the following 3 steps. 
 ##   1.) Subtract 1 from it. ( n = n - 1 )  , 
 ##   2.) If its divisible by 2, divide by 2. ( if n % 2 == 0 , then n = n / 2  )  , 
