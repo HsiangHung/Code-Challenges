63f74110f1f69b1c6d10ff6b751d5704a08a4bc1
Hsianghung2015 <hhhung@physics.utexas.edu>
2016-12-31 18:54:49 -0500

    leetcode

--- leetcode_solution/linked_list/Add_Two_Numbers.py
+++ leetcode_solution/linked_list/Add_Two_Numbers.py
@@ -12,47 +12,68 @@ class Solution(object):
         :type l2: ListNode
         :rtype: ListNode
         """
-        if l1 == None and l2 == None: return None
-        if l1 == None: return l2
-        if l2 == None: return l1
-        if l1.next == None and l2.next == None:
-            head, num = self.new_node(l1.val + l2.val)
-            if num == 1: head.next = ListNode(1)
-            return head
-        
-        head, num = self.new_node(l1.val + l2.val)
-        node = head
-        node1 = l1
-        node2 = l2
-        while node1.next != None and node2.next != None:
-            newNode, num = self.new_node(node1.next.val + node2.next.val + num)
-            node.next = newNode
-            node = newNode
-            node1 = node1.next
-            node2 = node2.next
-        #print node1.val, node2.val
-            
-        # --------------------------
-        if node2.next != None: node1 = node2
-        
-        while node1.next != None:
-            newNode, num = self.new_node(node1.next.val + num)
-            node.next = newNode
-            node = newNode
-            node1 = node1.next
-    
-        ## this considers [3,2,9,9,9] + [5,9,1] = [8,1,1,0,0,0,1], additional digits
-        if num == 1: newNode.next = ListNode(1)
-        
-        return head
-
-
-    def new_node(self, sum):
-        if sum >= 10:
-            newNode = ListNode(sum-10)
-            num = 1
+        len_1 = self.getLength(l1)
+        len_2 = self.getLength(l2)
+        if len_1 >= len_2:
+            long_ = l1
+            short_ = l2
         else:
-            newNode = ListNode(sum)
-            num = 0
-        return newNode, num
+            long_ = l2
+            short_ = l1
+            
+        ## at beginning, prepare the head node
+        head, next_digit = self.generateNodes(long_, short_, 0)
+        node = head
+        while short_.next != None:
+            long_ = long_.next
+            short_ = short_.next
+            newNode, next_digit = self.generateNodes(long_, short_, next_digit)
+            node.next = newNode
+            node = newNode
+            
+        ## now shorted list has been run over.
+        ## next we have to consider like [4,8,8] + [2,6,9] = [6,4,8,1]
+        ## also [4,8,8,9,9,9] + [2,6,9] = [6,4,8,0,0,0,1]
+        if next_digit == 0:
+            if long_.next != None: node.next = long_.next
+        else:
+            if long_.next == None:
+                node.next = ListNode(1)
+            else:
+                zeroNode = ListNode(0)
+                while long_.next != None:
+                    long_ = long_.next
+                    newNode, next_digit = self.generateNodes(long_, zeroNode, next_digit)
+                    node.next = newNode
+                    node = newNode
+                if next_digit != 0: node.next = ListNode(1)
+                    
+        return head
         
+        
+    def generateNodes(self, long_, short_, next_digit):
+        """
+        :type long_, short_: listNode
+        :type next_digit: int
+        :rtype: ListNode, int
+        """
+        sum_ = long_.val + short_.val+next_digit
+        next_digit = 0
+        if sum_ >= 10: 
+            next_digit = 1
+            sum_ -= 10
+        return ListNode(sum_), next_digit
+        
+        
+    def getLength(self, head):
+        """
+        get the length of the list
+        :type head: ListNode
+        :rtype: int
+        """
+        length = 1
+        node = head
+        while node.next != None:
+            node = node.next
+            length += 1
+        return length
