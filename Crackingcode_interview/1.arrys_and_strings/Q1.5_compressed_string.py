0e5554a976b82a528a34c93fed49859aa69f3b67
Hsianghung2015 <hhhung@physics.utexas.edu>
2016-12-31 18:54:40 -0500

    string

--- Crackingcode_interview/1.arrys_and_strings/Q1.5_compressed_string.py
+++ Crackingcode_interview/1.arrys_and_strings/Q1.5_compressed_string.py
@@ -5,18 +5,16 @@
 
 def compression_string(s):
     repeat = 1
-    compress_str = s[0]
-    for i in range(1,len(s)):
-        
-        if s[i] == s[i-1]: 
-            repeat += 1
-        elif s[i] != s[i-1]:
-            compress_str += str(repeat)+s[i]
+    new_str = ''
+    for i in range(1, len(s)):
+        if s[i] != s[i-1]:
+            new_str += s[i-1]+str(repeat)
             repeat = 1
-
-    compress_str += str(repeat)  
-            
-    return compress_str
+        else:
+            repeat +=1
+    new_str += s[len(s)-1]+str(repeat)
+    if len(new_str) > len(s): return s
+    return new_str
 
 
 print (compression_string('aabccccgdjjjhhh'))
