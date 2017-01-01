a7e911cf5a34064cf0d5bdc4ba5e7b99d9ec231f
Hsianghung2015 <hhhung@physics.utexas.edu>
2016-12-31 20:10:06 -0500

    linkedList

--- Crackingcode_interview/2.linked_lists/Q2.2_last_k_node.py
+++ Crackingcode_interview/2.linked_lists/Q2.2_last_k_node.py
@@ -1,13 +1,20 @@
 ## Q2.2: last-k element of the linked list
 #
 def last_k(head, k):
-    node = head
     length = 1
+    node = head
+    k_node = None
     while node.next != None:
-        if length == k: node_k = node
-        length +=1        
+        if length == k:
+            k_node = head
+        elif length > k:
+            k_node = k_node.next
+        length += 1
         node = node.next
-    
-    return node_k.val
+    if k_node != None: 
+        k_node =k_node.next
+        return k_node.val
+    return 'NO SUCH NODE'
+
     
 print (last_k(A, 1))
