8501479039f2f58c88b2425df184e0cb55805016
Hsianghung2015 <hhhung@physics.utexas.edu>
2016-12-31 19:10:59 -0500

    leetcode

--- leetcode_solution/tree/Binary_Tree_Right_Side_View.py
+++ leetcode_solution/tree/Binary_Tree_Right_Side_View.py
@@ -7,13 +7,14 @@
 #         self.right = None
 #
 class Solution(object):
+## this method considers to go through right DFS first
+##
     def rightSideView(self, root):
         """
         :type root: TreeNode
         :rtype: List[int]
         """
         if root == None: return []
-        
         self.views= []
         self.traverse(root, 1)
         return self.views
@@ -32,3 +33,27 @@ class Solution(object):
             if len(self.views) < depth+1: self.views.append(root.left.val)
             self.traverse(root.left, depth+1)
         
+        
+# ----------------------------------------------------------------------------        
+### method two:
+    def rightSideView2(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[int]
+        """
+        if not root: return []
+        
+        self.levels = []
+        self.traverse(root, 1)
+        return [x[0] for x in self.levels]
+        
+    def traverse(self, root, depth):
+        if len(self.levels) < depth:
+            self.levels.append([root.val])
+        else:
+            self.levels[depth-1].insert(0, root.val)
+            
+        if not root.left and not root.right: return
+    
+        if root.left != None: self.traverse(root.left, depth+1)
+        if root.right != None: self.traverse(root.right, depth+1)
