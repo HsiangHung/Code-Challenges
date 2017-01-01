aa4515f2cf53e3afdc00acc479ac63f2f0868aeb
Hsianghung2015 <hhhung@physics.utexas.edu>
2016-12-31 21:51:08 -0500

    tree

--- Crackingcode_interview/4.trees/Q4.3_generated_BST.py
+++ Crackingcode_interview/4.trees/Q4.3_generated_BST.py
@@ -6,23 +6,20 @@
 #         self.left = None
 #         self.right = None
 
-class Solution(object):
-    def sortedArrayToBST(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: TreeNode
-        """
-        if nums == []: return None
-        
-        return self.root(nums)
-        
-    def root(self, nums):
-        n = len(nums)
-        if n == 1: return TreeNode(nums[0])
-        
-        half = nums[int(n/2)]
-        node = TreeNode(half)
-        node.left = self.root(nums[:int(n/2)])
-        if  int(n/2)+1 < n:
-            node.right = self.root(nums[int(n/2)+1:])
-        return node
+def generate_BST(arr):
+    if len(arr) == 0: return None
+    if len(arr) == 1: return TreeNode(arr[0])
+    
+    mid = int(len(arr)/2)
+    root = TreeNode(arr[mid])
+    
+    Left = arr[:mid]
+    Right = arr[mid+1:]
+    root.left = generate_BST(Left)
+    root.right = generate_BST(Right)
+    return root
+
+root = generate_BST([1,2,5,10,20,21,46,100])
+
+## check with inorder:
+inOrder(root)
