016df7449d585022bbfff03eabd9dac4cadbea79
Hsianghung2015 <hhhung@physics.utexas.edu>
2018-03-01 09:04:02 -0800

    dynamical programming

---
+++ leetcode_solution/dynamic_programming/#122.Best_Time_Buy_and_Sell_Stock_II.py
@@ -0,0 +1,26 @@
+## [Leetcode#122] Best Time to Buy and Sell Stock II
+##
+##  total max profit is given by sum of max local profit
+##  when price goes down, we gain max profit by sell previous price to get max local
+##  profit.
+##
+class Solution(object):
+    def maxProfit(self, prices):
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
+        if len(prices) <= 1: return 0
+        
+        sum = 0 
+        local_max_profit = 0
+        local_min_price = prices[0]
+        for i in range(1, len(prices)):
+            if prices[i] < prices[i-1]:
+                sum += local_max_profit
+                local_min_price = prices[i]
+                local_max_profit = 0
+            else:
+                local_max_profit = max(local_max_profit, prices[i]-local_min_price)
+                
+        return sum + local_max_profit
