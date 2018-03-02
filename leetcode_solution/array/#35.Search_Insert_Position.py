146bc177c088382789c9c5c7d37e312cbb08cd1b
Hsianghung2015 <hhhung@physics.utexas.edu>
2018-03-02 07:48:03 -0800

    array

---
+++ leetcode_solution/array/#35.Search_Insert_Position.py
@@ -0,0 +1,15 @@
+## [Leetcode#35] Search Insert Position
+##
+##  
+class Solution(object):
+    def searchInsert(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        for i in range(len(nums)):
+            if nums[i] >= target:
+                return i
+                
+        return len(nums)
