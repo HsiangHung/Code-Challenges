348227172a1458c7027e017b9879f9f21ab99238
Hsianghung2015 <hhhung@physics.utexas.edu>
2018-03-18 14:36:44 -0700

    binary search

--- Crackingcode_interview/11.sorting_searching/binary_search.py
+++ Crackingcode_interview/11.sorting_searching/binary_search.py
@@ -1,26 +1,22 @@
 ## Binary Search
 ## return position index or return -1 if no target number exists
-def BS(array, target, start):
-
-    if len(array) == 1:
-        if array[0] != target: return -1
-        return start
-          
-    if len(array) == 2:
-        if array[0] == target: return start
-        if array[1] == target: return start+1
+def binary_search(offset, nums, target):
+    if len(nums) == 0:
         return -1
-
-    n = int(len(array)/2)
-    half = array[start+n]
     
-    if half == target:
-         return half+start
-    elif half > target:
-         return BS(array[:n], target, start)
-    elif half < target:
-         return BS(array[n+1:], target, start+n+1)
-
+    if len(nums) == 1:
+        if target != nums[0]:
+            return -1
+        else:
+            return offset
+        
+    middle = len(nums)//2
+    if nums[middle] == target:
+        return offset+middle
+    elif nums[middle] > target:
+        return binary_search(offset, nums[:middle] , target)
+    elif nums[middle] < target:
+        return binary_search(offset+middle+1, nums[middle+1:], target)
     
-
-print (BS([1,3,5,8,10,13], 0, 0))
+    
+print (binary_search(0, [0, 1, 2, 8, 13, 17, 19, 32, 42], 19))
