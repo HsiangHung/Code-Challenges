626212db431a01a0d342a0b577faec06d1b4870b
Hsianghung2015 <hhhung@physics.utexas.edu>
2018-03-24 20:52:55 -0700

    DFS

---
+++ leetcode_solution/DFS/#574.Friend_Circles.py
@@ -0,0 +1,32 @@
+# [#574] Friend Circles
+#
+#
+#
+class Solution(object):
+    def findCircleNum(self, M):
+        """
+        :type M: List[List[int]]
+        :rtype: int
+        """
+        Lrow = len(M)
+        Lcol = len(M[0])
+        
+        # This is the problem different from number of islands.
+        # it is equivalent to find which row or col indices have unique 
+        
+        n_circles = 0
+        for row in range(Lrow):
+            if M[row][row] == 1:
+                n_circles += 1
+                self.DFS(Lrow, Lcol, row, M)
+
+        return n_circles
+            
+    def DFS(self, Lrow, Lcol, row, M):
+        
+        if M[row][row] == 0: return
+        
+        M[row][row] = 0
+        for col in range(Lcol):
+            if M[row][col] == 1:
+                self.DFS(Lrow, Lcol, col, M)
