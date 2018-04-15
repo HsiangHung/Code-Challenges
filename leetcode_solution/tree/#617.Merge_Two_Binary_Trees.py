65be61e5c2bbbd7cd28527fd6da555091a76dffa
Hsianghung2015 <hhhung@physics.utexas.edu>
2018-04-15 10:40:48 -0700

    tree

--- leetcode_solution/tree/#617.Merge_Two_Binary_Trees.py
+++ leetcode_solution/tree/#617.Merge_Two_Binary_Trees.py
@@ -1,5 +1,6 @@
 # [#617] Merge Two Binary Trees
-#
+# 
+#  amazon
 #
 class Solution(object):
     def mergeTrees(self, t1, t2):
@@ -9,22 +10,21 @@ class Solution(object):
         :rtype: TreeNode
         """
         if not t1 and not t2: return None
-        if not t1: return t2
+        if not t1 and t2: return t2
         
         if t1 and t2:
             t1.val += t2.val
-            if t1.left:
+            
+            if t1.left and t2.left:
                 self.mergeTrees(t1.left, t2.left)
             elif not t1.left and t2.left:
                 t1.left = t2.left
-                
-            if t1.right:
+            
+            if t1.right and t2.right:
                 self.mergeTrees(t1.right, t2.right)
             elif not t1.right and t2.right:
                 t1.right = t2.right
-                
-        elif t1:
-            self.mergeTrees(t1.left, None)
-            self.mergeTrees(t1.right, None)
         
         return t1
+                
+       
