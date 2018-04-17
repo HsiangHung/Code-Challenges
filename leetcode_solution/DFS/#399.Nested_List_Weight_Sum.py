008c1826c7de0f28ca6586dacf77ceebc46dcbc8
Hsianghung2015 <hhhung@physics.utexas.edu>
2018-04-16 20:11:18 -0700

    DFS

---
+++ leetcode_solution/DFS/#399.Nested_List_Weight_Sum.py
@@ -0,0 +1,65 @@
+## [Leetcode#399] Nested List Weight Sum
+##
+#
+#  LinedIn
+#
+# """
+# This is the interface that allows for creating nested lists.
+# You should not implement it, or speculate about its implementation
+# """
+#class NestedInteger(object):
+#    def __init__(self, value=None):
+#        """
+#        If value is not specified, initializes an empty list.
+#        Otherwise initializes a single integer equal to value.
+#        """
+#
+#    def isInteger(self):
+#        """
+#        @return True if this NestedInteger holds a single integer, rather than a nested list.
+#        :rtype bool
+#        """
+#
+#    def add(self, elem):
+#        """
+#        Set this NestedInteger to hold a nested list and adds a nested integer elem to it.
+#        :rtype void
+#        """
+#
+#    def setInteger(self, value):
+#        """
+#        Set this NestedInteger to hold a single integer equal to value.
+#        :rtype void
+#        """
+#
+#    def getInteger(self):
+#        """
+#        @return the single integer that this NestedInteger holds, if it holds a single integer
+#        Return None if this NestedInteger holds a nested list
+#        :rtype int
+#        """
+#
+#    def getList(self):
+#        """
+#        @return the nested list that this NestedInteger holds, if it holds a nested list
+#        Return None if this NestedInteger holds a single integer
+#        :rtype List[NestedInteger]
+#        """
+
+class Solution(object):
+    def depthSum(self, nestedList):
+        """
+        :type nestedList: List[NestedInteger]
+        :rtype: int
+        """
+        return self.helper(1, nestedList)
+    
+    def helper(self, depth, nestedList):
+        depth_sum= 0
+        for nest in nestedList:
+            if nest.isInteger():
+                depth_sum += depth * nest.getInteger()
+            else:
+                depth_sum += self.helper(depth + 1, nest.getList())
+        
+        return depth_sum
