4a10b715d522cac026c7482873e17e468b5dba1b
Hsianghung2015 <hhhung@physics.utexas.edu>
2018-05-27 17:32:36 -0700

    linked list

---
+++ leetcode_solution/link list/#19.Remove_Nth_Node_From_End_of_List.py
@@ -0,0 +1,30 @@
+## [Leetcode#19] Remove Nth Node From End of List
+#
+#
+class Solution(object):
+    def removeNthFromEnd(self, head, n):
+        """
+        :type head: ListNode
+        :type n: int
+        :rtype: ListNode
+        """
+        if not head: return None
+        
+        node, length = head, 1
+        while node != None:
+            if length == n:
+                prev, target = None, head
+            elif length == n+1:
+                prev, target = head, target.next
+            elif length > n+1:
+                prev, target = prev.next, target.next
+            node = node.next
+            length += 1
+        
+        if prev == None:  ## to consider if n == len of linked list
+            return head.next
+        
+        prev.next = target.next
+            
+        return head
+            
