2f16178bcb888457354a30ae3c12347e4345c148
Hsianghung2015 <hhhung@physics.utexas.edu>
2018-06-01 21:28:01 -0700

    DFS

---
+++ leetcode_solution/DFS/#39.Combination_Sum.py
@@ -0,0 +1,25 @@
+## [Leetcode#39] Combination Sum
+#
+# Uber, Snapchat
+#
+class Solution(object):
+    def combinationSum(self, candidates, target):
+        """
+        :type candidates: List[int]
+        :type target: int
+        :rtype: List[List[int]]
+        """
+        if candidates == [] or min(candidates) > target: return []
+        
+        sol = []
+        for num in candidates:
+            if num == target:
+                sol.append([num])
+            else:
+                sol_set = self.combinationSum(candidates, target-num)
+                if len(sol_set) > 0:
+                    for x in sol_set:
+                        possible_sum = sorted([num]+x)
+                        if possible_sum not in sol:
+                            sol.append(possible_sum)
+        return sol
