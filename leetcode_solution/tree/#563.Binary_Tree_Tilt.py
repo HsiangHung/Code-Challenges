d6845a227b82ecfb463dca8ce6ddca3dd8e10147
Hsianghung2015 <hhhung@physics.utexas.edu>
2018-06-17 20:24:07 -0700

    tree

--- leetcode_solution/tree/#563.Binary_Tree_Tilt.py
+++ leetcode_solution/tree/#563.Binary_Tree_Tilt.py
@@ -1,24 +1,20 @@
 ## [Leetcode#563] Binary Tree Tilt
-##
+#
+#
+#
 class Solution(object):
     def findTilt(self, root):
         """
         :type root: TreeNode
         :rtype: int
         """
-        if not root: return 0
-        return self.traverse(root)[1]
-    
-    def traverse(self, root):
-        if not root.left and not root.right:
-            return root.val, 0
+        return self.get_tilt(root)[1]
 
-        left_sum, left_tilt = 0, 0
-        if root.left:
-            left_sum, left_tilt = self.traverse(root.left)
-            
-        right_sum, right_tilt = 0, 0
-        if root.right:
-            right_sum, right_tilt = self.traverse(root.right)
-            
-        return left_sum+right_sum+root.val, abs(left_sum - right_sum) + left_tilt + right_tilt
+    def get_tilt(self, root):
+        
+        if not root: return 0, 0
+        
+        left_sum, left_tilt = self.get_tilt(root.left)
+        right_sum, right_tilt = self.get_tilt(root.right)
+        
+        return left_sum + right_sum + root.val, left_tilt + right_tilt + abs(left_sum-right_sum)
