c0374e854fe6ed91e9944a6f98ef278df5ff43b0
Hsianghung2015 <hhhung@physics.utexas.edu>
2018-06-20 20:26:48 -0700

    tree

--- leetcode_solution/tree/#572.Subtree_Another_Tree.py
+++ leetcode_solution/tree/#572.Subtree_Another_Tree.py
@@ -14,43 +14,19 @@ class Solution(object):
         :rtype: bool
         """
         if not s and not t: return True
+        if not s or not t: return False
         
-        if s and t:
-            if s.val == t.val:
-                
-                # if s =t, start to compare. Even False, still continue search until end of s tree
-                if self.isSameTree(s, t):
-                    return True
-    
-            if s.left and s.right:
-                return self.isSubtree(s.left, t) or self.isSubtree(s.right, t)
-            elif s.left and not s.right:
-                return self.isSubtree(s.left, t)
-            elif not s.left and s.right:
-                return self.isSubtree(s.right, t)
-            else:
-                return False
-        else:
-            return False
-        
-        
-
-        
-    def isSameTree(self, s, t):
-        if s and t:
-            if s.val != t.val: return False
-            
-            if s.left and t.left and s.right and t.right:
-                return self.isSameTree(s.left, t.left) and self.isSameTree(s.right, t.right)
-            elif s.left and t.left and not s.right and not t.right:
-                return self.isSameTree(s.left, t.left)
-            elif not s.left and not t.left and s.right and t.right:
-                return self.isSameTree(s.right, t.right)
-            elif not s.left and not t.left and not s.right and not t.right:
+        if s.val == t.val:
+            if self.isSameTree(s, t):
                 return True
-            else:
-                return False
-        elif not s and not t:
-            return True
-        else:
-            return False
+            
+        return self.isSubtree(s.left, t) or self.isSubtree(s.right, t)
+        
+    
+    def isSameTree(self, p, q):
+        if not p and not q: return True
+        if not p or not q: return False
+        
+        if p.val != q.val: return False
+        
+        return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
