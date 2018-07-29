fc8c8263783b9dd0ff74cb7ed6418eea101b1724
Hsianghung2015 <hhhung@physics.utexas.edu>
2018-07-29 16:45:34 -0700

    dynamical programming

---
+++ leetcode_solution/dynamical programming/#78.Subsets.py
@@ -0,0 +1,29 @@
+## [# 78] Subsets
+#
+#
+#
+class Solution(object):
+    def subsets(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        if len(nums) == 0: return []
+        
+        dp = {0: [], 1: [[x] for x in nums]}
+
+        set_len = 2
+        while set_len <= len(nums):
+            
+            dp[set_len] = []
+            for num in nums:
+                for subset in dp[set_len-1]:
+                    if num > subset[-1]:
+                        new_subset = subset + [num]
+                        dp[set_len].append(new_subset)
+            set_len += 1
+        
+        subsets = []
+        for set_len in dp:
+            subsets += dp[set_len]
+        return [[]] + subsets
