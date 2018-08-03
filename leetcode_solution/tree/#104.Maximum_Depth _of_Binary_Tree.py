6573e9a5da083b29be4d2a1bfd6bd341e8df86ee
Triple H <Hsiang.Hung2015@gmail.com>
2018-08-02 20:55:20 -0700

    tree

--- leetcode_solution/tree/#104.Maximum_Depth _of_Binary_Tree.py
+++ leetcode_solution/tree/#104.Maximum_Depth _of_Binary_Tree.py
@@ -14,12 +14,5 @@ class Solution(object):
         """
         if not root: return 0
         
-        max_depth = 1
-        if root.left:
-            max_depth = max(max_depth, self.maxDepth(root.left)+1)
-            
-        if root.right:
-            max_depth = max(max_depth, self.maxDepth(root.right)+1)
-        
-        return max_depth      
+        return max(self.maxDepth(root.left), self.maxDepth(root.right))+1
 
