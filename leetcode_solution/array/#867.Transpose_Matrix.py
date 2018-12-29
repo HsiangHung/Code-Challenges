49c9fe836613aa054a99f7e83bdcd1668eccd187
Triple H <Hsiang.Hung2015@gmail.com>
2018-12-28 18:41:59 -0800

    array

---
+++ leetcode_solution/array/#867.Transpose_Matrix.py
@@ -0,0 +1,17 @@
+## [Leetcode#867] Transpose Matrix
+##
+##  
+class Solution(object):
+    def transpose(self, A):
+        """
+        :type A: List[List[int]]
+        :rtype: List[List[int]]
+        """
+        B = []
+        for i in range(len(A[0])):
+            row = []
+            for j in range(len(A)):
+                row.append(A[j][i])
+            B.append(row)
+            
+        return B
