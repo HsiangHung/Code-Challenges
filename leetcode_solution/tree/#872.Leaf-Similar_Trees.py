8653a78cd4248a1b18fb8d0c1f5173141f54ae43
Triple H <Hsiang.Hung2015@gmail.com>
2019-11-14 07:29:17 -0800

    Update #872.Leaf-Similar_Trees.py

--- leetcode_solution/tree/#872.Leaf-Similar_Trees.py
+++ leetcode_solution/tree/#872.Leaf-Similar_Trees.py
@@ -9,23 +9,22 @@ class Solution(object):
         :type root2: TreeNode
         :rtype: bool
         """
-        if not root1 and not root2: return True
+        if not root1 and not root2: return True        
         if not root1 or not root2: return False
-        self.leaves = {1: [], 2: []}
-        self.traversal(root1, 1)
-        self.traversal(root2, 2)
-        return self.leaves[1] == self.leaves[2]
+        return self.trasverse(root1) == self.trasverse(root2)
         
+    def trasverse(self, root):
+        if not root.left and not root.right: 
+            return [root.val]
         
-    def traversal(self, root, tree):
-        if not root.left and not root.right:
-            self.leaves[tree].append(root.val)
-            return
-
+        sequence = []
+        
         if root.left:
-            self.traversal(root.left, tree)
+            sequence += self.trasverse(root.left)
             
         if root.right:
-            self.traversal(root.right, tree)
+            sequence += self.trasverse(root.right)
+            
+        return sequence
                 
         
