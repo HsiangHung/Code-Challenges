a637010d0ba73ecbf86a1237da10ea31660af830
Triple H <Hsiang.Hung2015@gmail.com>
2020-08-23 21:45:17 -0700

    #98

--- leetcode_solution/tree/#98.Validate_Binary_Search_Tree.py
+++ leetcode_solution/tree/#98.Validate_Binary_Search_Tree.py
@@ -14,29 +14,26 @@ class Solution(object):
         #
         if not root: return True
         if root.left:
-            min_left, max_left = self.traversal(root.left)
+            min_left, max_left = self.DFS(root.left)
             if max_left == None or root.val <= max_left: return False
             
         if root.right:
-            min_right, max_right = self.traversal(root.right)
+            min_right, max_right = self.DFS(root.right)
             if max_right == None or root.val >= min_right: return False
             
         return True
     
     
-    def traversal(self, root):
-        if not root.left and not root.right:
-            return root.val, root.val
+    def DFS(self, root):
+        if not root.left and not root.right: return root.val, root.val  
         
-        min_val, max_val = root.val, root.val
+        min_left, max_right = root.val, root.val
         if root.left:
-            min_left, max_left = self.traversal(root.left)
+            min_left, max_left = self.DFS(root.left)
             if max_left == None or root.val <= max_left: return False, None
-            min_val = min(min_left, min_val)
             
         if root.right:
-            min_right, max_right = self.traversal(root.right)
+            min_right, max_right = self.DFS(root.right)
             if max_right == None or root.val >= min_right: return False, None
-            max_val = max(max_right, max_val)
         
-        return min_val, max_val
+        return min_left, max_right
