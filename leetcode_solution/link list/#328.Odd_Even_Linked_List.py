8358e95425ee1fa957fcdde99d93130394cdadbc
Triple H <Hsiang.Hung2015@gmail.com>
2020-08-23 20:06:24 -0700

    #328

---
+++ leetcode_solution/link list/#328.Odd_Even_Linked_List.py
@@ -0,0 +1,35 @@
+## [Leetcode#328] Odd Even Linked List
+# 
+#
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def oddEvenList(self, head: ListNode) -> ListNode:
+        
+        if head == None or head.next == None or head.next.next == None: return head
+        
+        odd, even = head, head.next
+        odd_head, even_head = odd, even
+        
+        node, nextNode, nextNextNode = head, head.next, head.next.next
+        pos = 0
+        while nextNode != None and nextNextNode != None:
+            pos += 1
+            if pos % 2 == 1:
+                odd.next = nextNextNode
+                odd = odd.next
+            else:
+                even.next = nextNextNode
+                even = even.next
+            
+            node = nextNode
+            nextNode = nextNextNode
+            nextNextNode = nextNextNode.next
+                  
+        even.next = None
+        odd.next = even_head
+        
+        return odd_head
