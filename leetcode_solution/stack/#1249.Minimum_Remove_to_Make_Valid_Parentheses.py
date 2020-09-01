39d815613732cb01ea9f622c149b5b0c7041098d
Triple H <Hsiang.Hung2015@gmail.com>
2020-09-01 00:12:22 -0700

    #1249 stack

---
+++ leetcode_solution/stack/#1249.Minimum_Remove_to_Make_Valid_Parentheses.py
@@ -0,0 +1,25 @@
+# 1249. Minimum Remove to Make Valid Parentheses
+#
+class Solution:
+    def minRemoveToMakeValid(self, s: str) -> str:
+        '''
+        The trick here is we store all "(" and ")" position indices, and 
+        if only if "(" + ")" we pop stack and index array. The remaining indices are 
+        invalid paraenthese needed to remove.
+        '''
+        stack = []
+        idx = []
+        for i in range(len(s)):
+            if s[i] == "(" or s[i] == ")":
+                if len(stack) == 0:
+                    idx.append(i)
+                    stack.append(s[i])
+                else:
+                    if stack[-1] == "(" and s[i] == ")":
+                        stack.pop()
+                        idx.pop()
+                    else:
+                        stack.append(s[i])
+                        idx.append(i)
+                            
+        return "".join([s[i] for i in range(len(s)) if i not in set(idx)])
