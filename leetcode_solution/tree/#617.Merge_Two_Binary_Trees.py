f27743df734c47578adfaa9df7b156c368ba492d
Triple H <Hsiang.Hung2015@gmail.com>
2020-09-21 10:32:32 -0700

    #617

--- leetcode_solution/tree/#617.Merge_Two_Binary_Trees.py
+++ leetcode_solution/tree/#617.Merge_Two_Binary_Trees.py
@@ -9,21 +9,16 @@ class Solution(object):
         :type t2: TreeNode
         :rtype: TreeNode
         """
-        if not t1 and not t2: return None
-        if not t1 and t2: return t2
+        if not t1 and not t2: return
         
-        if t1 and t2:
-            t1.val += t2.val
-            
-            if t1.left and t2.left:
-                self.mergeTrees(t1.left, t2.left)
-            elif not t1.left and t2.left:
-                t1.left = t2.left
-            
-            if t1.right and t2.right:
-                self.mergeTrees(t1.right, t2.right)
-            elif not t1.right and t2.right:
-                t1.right = t2.right
+        if not t2: return t1
+        
+        if not t1: return t2
+        
+        t1.val += t2.val
+                
+        t1.left = self.mergeTrees(t1.left, t2.left)
+        t1.right = self.mergeTrees(t1.right, t2.right)
         
         return t1
                 
