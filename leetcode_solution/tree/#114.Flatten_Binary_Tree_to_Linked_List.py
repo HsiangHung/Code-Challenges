175183bd95b7519fdce09c3b5e584ae2385a8151
Triple H <Hsiang.Hung2015@gmail.com>
2020-10-29 00:06:12 -0700

    Update #114.Flatten_Binary_Tree_to_Linked_List.py

--- leetcode_solution/tree/#114.Flatten_Binary_Tree_to_Linked_List.py
+++ leetcode_solution/tree/#114.Flatten_Binary_Tree_to_Linked_List.py
@@ -13,19 +13,32 @@ class Solution(object):
                 
         left, right = root.left, root.right
         
-        if left and right:
+        if left:
             root.right = left
             root.left = None
-            left = self.flatten(left)            
-            left.right = right
-            right = self.flatten(right)
-            return right
-        elif left and not right:
-            root.right = left
-            root.left = None
-            left = self.flatten(left)
-            return left
-        elif not left and right:  # note, this is necessary since we need to come back last node for later concat.
-            return self.flatten(right)
+            left = self.flatten(left) 
+            if right:
+                left.right = right
+                right = self.flatten(right)
+                return right
+            else:
+                return left
+        else:
+            if right: return self.flatten(right)
+
+        # if left and right:
+        #     root.right = left
+        #     root.left = None
+        #     left = self.flatten(left)            
+        #     left.right = right
+        #     right = self.flatten(right)
+        #     return right
+        # elif left and not right:
+        #     root.right = left
+        #     root.left = None
+        #     left = self.flatten(left)
+        #     return left
+        # elif not left and right:  # note, this is necessary since we need to come back last node for later concat.
+        #     return self.flatten(right)
         
         return right               
