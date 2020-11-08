cae89576d305fb33559bc7c474faeecb53e23614
Triple H <Hsiang.Hung2015@gmail.com>
2020-11-07 21:03:35 -0800

    Create #997.Find_the_Town_Judge.py

---
+++ leetcode_solution/hash table/#997.Find_the_Town_Judge.py
@@ -0,0 +1,18 @@
+# # 997. Find the Town Judge
+#
+class Solution:
+    def findJudge(self, N: int, trust: List[List[int]]) -> int:
+        
+        if N == 1: return 1
+        
+        trusted = {}
+        trusting = set({})
+        for a, b in trust:
+            trusted[b] = trusted.get(b, []) + [a]
+            trusting.add(a)
+            
+        for judge in trusted:
+            if len(set(trusted[judge])) == N-1 and judge not in trusting: 
+                return judge
+            
+        return -1
