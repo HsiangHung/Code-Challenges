06a988ee2d4fe0492d8d5729c0019f9887ad1002
Triple H <Hsiang.Hung2015@gmail.com>
2020-11-09 20:27:22 -0800

    Update #416.Partition_Equal_Subset_Sum.py

--- leetcode_solution/hash table/#416.Partition_Equal_Subset_Sum.py
+++ leetcode_solution/hash table/#416.Partition_Equal_Subset_Sum.py
@@ -5,17 +5,25 @@ class Solution:
         '''
         time complexity O(n*sum), space O(sum)
         http://bookshadow.com/weblog/2016/10/09/leetcode-partition-equal-subset-sum/
+
+        e.g. nums = [1,5,11,5]
+        initialize dp = {0}
+        then we have num    dp
+                      1     {0, 1}
+                      5     {0, 1, 5, 6}
+                     11     {0, 1, 5, 6, 11, 12, 16, 17}
+                      5     {0, 1, 5, 6, 10, 11, 12, 16, 17, 21, 22}
         '''
         total_sum = sum(nums)
         
         if total_sum % 2 == 1: return False
         
-        sum_set = set({0})
+        dp = set({0})
         for num in nums:
-            for x in sum_set.copy():  ## note, set.copy(), otherwise chaing set size.
-                sum_set.add(x+num)
+            for x in dp.copy():  ## note, set.copy(), otherwise chaing set size.
+                dp.add(x+num)
                 
-        return total_sum/2 in sum_set
+        return total_sum/2 in dp
             
         
         
