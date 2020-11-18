dd08c8eccac1c453603cf41fddb450d28bb06518
Triple H <Hsiang.Hung2015@gmail.com>
2020-11-18 14:32:40 -0800

    Create #150.Evaluate_Reverse_Polish_Notation.py

---
+++ leetcode_solution/stack/#150.Evaluate_Reverse_Polish_Notation.py
@@ -0,0 +1,28 @@
+# 150. Evaluate Reverse Polish Notation
+#
+class Solution:
+    def evalRPN(self, tokens: List[str]) -> int:
+        
+        operators = ("+", "-", "*", "/")
+        
+        stack = []
+        for x in tokens:
+            if x not in operators:
+                stack.append(x)
+            else:
+                b = stack.pop()
+                a = stack.pop()
+                stack.append(self.operation(a, b, x))
+        
+        return int(stack[-1])
+                
+                
+    def operation(self, a, b, operation):
+        if operation == "+":
+            return int(a) + int(b)
+        elif operation == "-":
+            return int(a) - int(b)
+        elif operation == "*":
+            return int(a) * int(b)
+        else:
+            return int(a) / int(b)
