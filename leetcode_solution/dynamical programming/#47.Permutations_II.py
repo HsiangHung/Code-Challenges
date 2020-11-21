2ebc6f0f75ef6732e8214cbf657ed71f078fbadc
Triple H <Hsiang.Hung2015@gmail.com>
2020-11-21 01:02:24 -0800

    medium

--- leetcode_solution/dynamical programming/#47.Permutations_II.py
+++ leetcode_solution/dynamical programming/#47.Permutations_II.py
@@ -1,4 +1,5 @@
-# # 47. Permutations II
+# 47. Permutations II (medium)
+# https://leetcode.com/problems/permutations-ii/
 #
 class Solution:
     def permuteUnique(self, nums: List[int]) -> List[List[int]]:
