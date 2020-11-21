9c9478d0c0167660b4f46b26d99482517094aba0
Triple H <Hsiang.Hung2015@gmail.com>
2020-11-21 12:39:46 -0800

    medium

--- leetcode_solution/dynamical programming/#91.Decode_Ways.py
+++ leetcode_solution/dynamical programming/#91.Decode_Ways.py
@@ -1,5 +1,7 @@
-# #91. Decode Ways
+#  91. Decode Ways (medium)
+#  https://leetcode.com/problems/decode-ways/
 # 
+#
 class Solution:
     def numDecodings(self, s: str) -> int:
         '''
@@ -12,12 +14,6 @@ class Solution:
              "222":  dp[3] = (dp[2] + "2") & (dp[1] + "22") = 2 + 1 = 3
              "2225": dp[4] = (dp[3] + "5") & (dp[2] + "25") = 3 + 2 = 5
              
-        e.g. s = "2265":
-             dp[0] = dp[1] = 1 
-             "22":   dp[2] = (dp[1] + "2") & (dp[0] + "22") = 1 + 1 = 2
-             "226":  dp[3] = (dp[2] + "6") & (dp[1] + "26") = 2 + 1 = 3
-             "2265": dp[4] = (dp[3] + "5") & (dp[2] + "65") = 3 + 0 = 3
- 
         e.g. s = "2265":
              dp[0] = dp[1] = 1 
              "22":   dp[2] = (dp[1] + "2") & (dp[0] + "22") = 1 + 1 = 2
