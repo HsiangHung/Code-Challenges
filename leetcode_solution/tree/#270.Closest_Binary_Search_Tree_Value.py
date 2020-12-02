4095688284dd29d490803d283f4a536c8ee35ab1
Triple H <Hsiang.Hung2015@gmail.com>
2020-12-02 09:18:45 -0800

    easy

--- leetcode_solution/tree/#270.Closest_Binary_Search_Tree_Value.py
+++ leetcode_solution/tree/#270.Closest_Binary_Search_Tree_Value.py
@@ -1,4 +1,6 @@
-# # 270. Closest Binary Search Tree Value
+#  270. Closest Binary Search Tree Value (easy)
+#  https://leetcode.com/problems/closest-binary-search-tree-value/
+#
 #
 # Definition for a binary tree node.
 # class TreeNode:
@@ -24,5 +26,43 @@ class Solution:
             val = self.closestValue(root.right, target)
             if abs(val-target) < abs(output-target): output = val  
                 
-        
         return output
+
+#
+#  The following is an optimal solution to search closest node.
+#  Since it is a BST, we only search left if the current node root.val - target > 0
+#  Similarly, only search right if root.val - target < 0
+#
+class OptSolution:
+    def closestValue(self, root: TreeNode, target: float) -> int:
+        
+        self.closest = root.val
+        self.min_diff = root.val-target
+        
+        self.DFS(target, root)
+        return self.closest
+        
+    def DFS(self, target, root):
+        if not root: return
+        
+        diff = root.val-target
+        
+        if abs(diff) < abs(self.min_diff):
+            self.closest = root.val
+            self.min_diff = diff
+            
+        if self.min_diff == 0: return
+        
+        if root.left and root.right:
+            if diff > 0: 
+                self.DFS(target, root.left)
+            elif diff < 0: 
+                self.DFS(target, root.right)
+        elif root.left:
+            self.DFS(target, root.left)
+        elif root.right:
+            self.DFS(target, root.right)
+        
+
+
+
