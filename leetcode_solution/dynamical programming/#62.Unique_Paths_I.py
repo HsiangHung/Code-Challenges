e3fa0d666459cf1854f14e8161ce227fad995847
Triple H <Hsiang.Hung2015@gmail.com>
2020-12-05 12:23:52 -0800

    medium

--- leetcode_solution/dynamical programming/#62.Unique_Paths_I.py
+++ leetcode_solution/dynamical programming/#62.Unique_Paths_I.py
@@ -4,32 +4,33 @@
 #
 class Solution(object):
     def uniquePaths(self, m, n):
-        """
-        :type m: int
-        :type n: int
-        :rtype: int
-        """
-        #return self.recursion_count(m, n)
-        dp = [[1]*m]+[[0]*m]*(n-1)
-        for i in range(n):
-            dp[i][0] = 1
-         
-        for y in range(1, n):
-            for x in range(1, m):
-                dp[y][x] = dp[y-1][x] + dp[y][x-1]
+        '''
+        https://www.youtube.com/watch?v=OIc0mHgHUww
+        
+        Given a root and left/right sub BST, the number of various combination is
+        
+        C = C_l*C_r
+        
+        e.g. n=4, root can be 1,2,3,4
+        root = 1: C_l = [];  C_r = [2,3,4].....
+        root = 2: C_l = [1]; C_r = [3,4].....
+        root = 3: C_l = [1,2]; C_r = [4].....
+        root = 4: C_l = [1,2,3]; C_r = [].....
+        '''
+        
+        dp = {0: 1, 1: 1}
+        
+        i = 2
+        while i <= n:
             
-        return dp[n-1][m-1]
-        
-        
-        
-    def recursion_count(self, m, n):
-        '''
-        This method will exceed time limit by Leetcode, but works
-        '''
-        if m == 1 and n == 1: return 1
-        num_paths = 0
-        if m > 1:
-            num_paths += self.uniquePaths(m-1, n)            
-        if n > 1:
-            num_paths += self.uniquePaths(m, n-1)
-        return num_paths
+            dp[i] = 0
+            for x in range(i):
+                root = x + 1
+                
+                left, right = root-1, i - root  # left and right are the number of nodes of subtree. 
+                print (left, right)
+                dp[i] += dp[left]*dp[right]
+            
+            i += 1
+                
+        return dp[n]        
