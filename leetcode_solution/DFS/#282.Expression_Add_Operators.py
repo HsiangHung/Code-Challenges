efc6d9e1f14c32522c2e1a0d735c91e4f90dd6b7
Triple H <Hsiang.Hung2015@gmail.com>
2020-12-04 20:18:18 -0800

    hard

---
+++ leetcode_solution/DFS/#282.Expression_Add_Operators.py
@@ -0,0 +1,60 @@
+#  282. Expression Add Operators (hard)
+#  https://leetcode.com/problems/expression-add-operators/
+#
+class Solution:
+    def addOperators(self, num: str, target: int) -> List[str]:
+        '''
+        https://wdxtub.com/interview/14520604912510.html
+        http://bookshadow.com/weblog/2015/09/16/leetcode-expression-add-operators/
+        
+        e.g. num = "1234", need to go 
+        ["1","234"], ["1","2","34"],["1","2","3","4"]
+        ["12","34"], ["12","3","4"] ....
+        and insert opertation "+", "-", "*" between any two numbers
+        
+        NOTE: for "+", "-" DFS is straightforward. But for "*", we need to keep track last number which
+        has "+"/"-", and do muplitcation operation on the number. i.e. 
+        
+        ["1","+","2"], result = 1+2=3
+        if comes "*","3", we need to update as => ["1","+","2*3"] = ["1","+","6"]
+        
+        Three to store, expression, stack for real operation (keep track last number) and real result.
+        e.g. ex = "1+2*3",   stack = ["1","+","6"],  result =  7
+             ex = "1+2*3*4", stack = ["1","+","24"], result = 25
+        '''
+        self.ans = []
+        if num == "": return self.ans       
+        
+        for i in range(len(num)):
+            if len(num[:i+1]) > 1 and num[0] == "0": continue
+            self.DFS(num[i+1:], target, num[:i+1], [int(num[:i+1])], int(num[:i+1]))
+        return self.ans
+                
+                
+    def DFS(self, num, target, ex, stack, result):
+        
+        if num == "":
+            if result == target: 
+                self.ans.append(ex)
+            return
+        
+        for i in range(len(num)):
+                        
+            if len(num[:i+1]) > 1 and num[0] == "0": continue   ## trick: for example "02", then we skip
+                            
+            self.DFS(num[i+1:], target, ex+"+"+num[:i+1], stack+["+", num[:i+1]], result+int(num[:i+1]))
+            self.DFS(num[i+1:], target, ex+"-"+num[:i+1], stack+["-", num[:i+1]], result-int(num[:i+1]))
+                        
+            stack2 = stack[:]    # NOTE here we need to assign another stack2, if directly use stack
+                                 # in "*" DFS, it will impact values for other recursion processes.
+            if len(stack2) >=3:  # case of stack = ["1","+","2"]
+                op, x = stack2[-2], int(stack2[-1])
+                stack2[-1] = x*int(num[:i+1])
+                if op == "+":
+                    self.DFS(num[i+1:], target, ex+"*"+num[:i+1], stack2, result-x+x*int(num[:i+1]))
+                elif op == "-":
+                    self.DFS(num[i+1:], target, ex+"*"+num[:i+1], stack2, result+x-x*int(num[:i+1]))
+            else:                # case of stack = ["1"], no operation needs to worry
+                x = int(stack2[-1])
+                stack2[-1] = x*int(num[:i+1])
+                self.DFS(num[i+1:], target, ex+"*"+num[:i+1], stack2, result-x+x*int(num[:i+1]))
