0750eeb70dfd850b02c7f60a1b9b0421145f4af2
Triple H <Hsiang.Hung2015@gmail.com>
2020-12-07 10:21:43 -0800

    easy

--- leetcode_solution/hash table/#997.Find_the_Town_Judge.py
+++ leetcode_solution/hash table/#997.Find_the_Town_Judge.py
@@ -1,4 +1,5 @@
-# # 997. Find the Town Judge
+#  997. Find the Town Judge (easy)
+#  https://leetcode.com/problems/find-the-town-judge/
 #
 class Solution:
     def findJudge(self, N: int, trust: List[List[int]]) -> int:
