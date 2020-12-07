c5db1172462d97bada21d78a1c8f3f18fe7d88a9
Triple H <Hsiang.Hung2015@gmail.com>
2020-12-07 10:53:50 -0800

    medium

--- leetcode_solution/tree/#98.Validate_Binary_Search_Tree.py
+++ leetcode_solution/tree/#98.Validate_Binary_Search_Tree.py
@@ -1,4 +1,5 @@
-## [Leetcode#98] Validate Binary Search Tree
+#  98. Validate Binary Search Tree (medium)
+#  https://leetcode.com/problems/validate-binary-search-tree/
 #
 #  facebook, microsoft, amazon, bloomberg
 #
@@ -13,27 +14,22 @@ class Solution(object):
         #                         maximum of left subtree should be less than the root
         #
         if not root: return True
-        if root.left:
-            min_left, max_left = self.DFS(root.left)
-            if max_left == None or root.val <= max_left: return False
-            
-        if root.right:
-            min_right, max_right = self.DFS(root.right)
-            if max_right == None or root.val >= min_right: return False
-            
+        a, b = self.DFS(root)
+        if a is False or b is False: return False
         return True
     
     
     def DFS(self, root):
-        if not root.left and not root.right: return root.val, root.val  
+        if not root.left and not root.right: 
+            return root.val, root.val  
         
         min_left, max_right = root.val, root.val
         if root.left:
             min_left, max_left = self.DFS(root.left)
-            if max_left == None or root.val <= max_left: return False, None
+            if max_left is False or root.val <= max_left: return False, False
             
         if root.right:
             min_right, max_right = self.DFS(root.right)
-            if max_right == None or root.val >= min_right: return False, None
+            if max_right is False or root.val >= min_right: return False, False
         
         return min_left, max_right
