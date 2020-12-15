be1a1079703d251d7be3d68a1459a8d8fa32321c
Triple H <Hsiang.Hung2015@gmail.com>
2020-12-14 19:27:34 -0800

    medium

--- leetcode_solution/tree/#987.Vertical_Order_Traversal_of_a_Binary_Tree.py
+++ leetcode_solution/tree/#987.Vertical_Order_Traversal_of_a_Binary_Tree.py
@@ -1,4 +1,5 @@
-# #987. Vertical Order Traversal of a Binary Tree
+#  987. Vertical Order Traversal of a Binary Tree (medium)
+#  https://leetcode.com/problems/vertical-order-traversal-of-a-binary-tree/
 #
 # Definition for a binary tree node.
 # class TreeNode:
@@ -6,7 +7,33 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
-class Solution:
+class BFS_Solution:
+    def verticalTraversal(self, root: TreeNode) -> List[List[int]]:
+        
+        if not root: return []
+        queue = [(root, 0, 0)]
+        position = {}
+        while queue:
+            node, x, y = queue.pop(0)
+            
+            if x not in position:
+                position[x] = {y: [node.val]}
+            else:
+                position[x][y] = position[x].get(y, []) + [node.val]
+            
+            if node.left: queue.append((node.left, x-1, y+1))
+            if node.right: queue.append((node.right, x+1, y+1))
+                
+        ans = []
+        for x in sorted(position.keys()):
+            views = []
+            for y in sorted(position[x].keys()):
+                views += sorted(position[x][y])
+            ans.append(views)
+        return ans
+#
+#
+class DFS_Solution:
     def __init__(self):
         self.position = {}
         
