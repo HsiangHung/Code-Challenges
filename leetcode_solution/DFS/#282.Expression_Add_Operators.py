865378ff585862629eea4434da1671b9162003bf
Triple H <Hsiang.Hung2015@gmail.com>
2020-12-17 19:59:10 -0800

    hard

--- leetcode_solution/DFS/#282.Expression_Add_Operators.py
+++ leetcode_solution/DFS/#282.Expression_Add_Operators.py
@@ -41,20 +41,22 @@ class Solution:
         for i in range(len(num)):
                         
             if len(num[:i+1]) > 1 and num[0] == "0": continue   ## trick: for example "02", then we skip
-                            
-            self.DFS(num[i+1:], target, ex+"+"+num[:i+1], stack+["+", num[:i+1]], result+int(num[:i+1]))
-            self.DFS(num[i+1:], target, ex+"-"+num[:i+1], stack+["-", num[:i+1]], result-int(num[:i+1]))
+            
+            y = num[:i+1]
+
+            self.DFS(num[i+1:], target, ex+"+"+y, stack+["+", int(y)], result+int(y))
+            self.DFS(num[i+1:], target, ex+"-"+y, stack+["-", int(y)], result-int(y))
                         
             stack2 = stack[:]    # NOTE here we need to assign another stack2, if directly use stack
                                  # in "*" DFS, it will impact values for other recursion processes.
-            if len(stack2) >=3:  # case of stack = ["1","+","2"]
+            if len(stack2) >=2:  # case of stack = ["1","+","2"]
                 op, x = stack2[-2], int(stack2[-1])
-                stack2[-1] = x*int(num[:i+1])
+                stack2[-1] = x*int(y)
                 if op == "+":
-                    self.DFS(num[i+1:], target, ex+"*"+num[:i+1], stack2, result-x+x*int(num[:i+1]))
+                    self.DFS(num[i+1:], target, ex+"*"+y, stack2, result-x+x*int(y))
                 elif op == "-":
-                    self.DFS(num[i+1:], target, ex+"*"+num[:i+1], stack2, result+x-x*int(num[:i+1]))
+                    self.DFS(num[i+1:], target, ex+"*"+y, stack2, result+x-x*int(y))
             else:                # case of stack = ["1"], no operation needs to worry
                 x = int(stack2[-1])
-                stack2[-1] = x*int(num[:i+1])
-                self.DFS(num[i+1:], target, ex+"*"+num[:i+1], stack2, result-x+x*int(num[:i+1]))
+                stack2[-1] = x*int(y)
+                self.DFS(num[i+1:], target, ex+"*"+y, stack2, result-x+x*int(y))
