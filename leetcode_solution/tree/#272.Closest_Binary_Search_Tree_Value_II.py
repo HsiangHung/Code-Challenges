bcf2d11a578087aa1c6422b16103af406bb9b084
Triple H <Hsiang.Hung2015@gmail.com>
2020-12-28 21:32:30 -0800

    hard

--- leetcode_solution/tree/#272.Closest_Binary_Search_Tree_Value_II.py
+++ leetcode_solution/tree/#272.Closest_Binary_Search_Tree_Value_II.py
@@ -1,4 +1,5 @@
-# # 272. Closest Binary Search Tree Value II
+#  272. Closest Binary Search Tree Value II (hard)
+#  https://leetcode.com/problems/closest-binary-search-tree-value-ii/
 #
 # Definition for a binary tree node.
 # class TreeNode:
@@ -8,7 +9,7 @@
 #         self.right = right
 class Solution:
     def __init__(self):
-        self.diff = []
+        self.queue = []
         
     def closestKValues(self, root: TreeNode, target: float, k: int) -> List[int]:
         '''
@@ -16,28 +17,33 @@ class Solution:
         self.diff is order by diff
         '''
         self.DFS(root, target, k)
-        return [int(target+x[0]*x[1]) for x in self.diff]
-    
-    def DFS(self, root, target, k):
-        self.insert(root.val, target, k)
-        if root.left: self.closestKValues(root.left, target, k)
-        if root.right: self.closestKValues(root.right, target, k)
-
-    def insert(self, val, target, k):
+        return [x[1] for x in self.queue[-k:]]
         
-        if len(self.diff) == 0: 
-            sgn = 1 if val > target else -1
-            self.diff.append((abs(val-target), sgn))
-        else:
-            sgn = 1 if val > target else -1
-            if abs(val-target) <= self.diff[0][0]:
-                self.diff.insert(0, (abs(val-target), sgn))
-            elif abs(val-target) < self.diff[-1][0]:
-                i = 0
-                while abs(val-target) >= self.diff[i][0]:
-                    i += 1
-                self.diff.insert(i, (abs(val-target), sgn))
+    def DFS(self, root, target, k):
+        if not root: return
+        if len(self.queue) >= k:
+            if abs(root.val-target) >= self.queue[-1][0]:
+                if root.val > target:
+                    self.DFS(root.left, target, k)
+                else:
+                    self.DFS(root.right, target, k)
+                return 
+            elif abs(root.val-target) < self.queue[0][0]:
+                self.queue.insert(0, (abs(root.val-target), root.val))
+                self.queue.pop()
             else:
-                self.diff.append((abs(val-target), sgn))
-
-            self.diff = self.diff[:k]
+                self.insert(root, target, k)
+        else:
+            self.insert(root, target, k)
+    
+        self.DFS(root.left, target, k), self.DFS(root.right, target, k)
+            
+    def insert(self, root, target, k):
+        i = 0
+        while i <= len(self.queue)-1 and abs(root.val-target) >= self.queue[i][0]:
+            i += 1
+        self.queue.insert(i, (abs(root.val-target), root.val))
+        self.queue = self.queue[:k]
+        
+        
+        
