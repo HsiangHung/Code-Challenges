eaf254ddd0daa45260c9aacc071c07dac134998e
Triple H <Hsiang.Hung2015@gmail.com>
2021-01-02 12:32:36 -0800

    medium

--- leetcode_solution/BFS/#499.The_Maze_III.py
+++ leetcode_solution/BFS/#499.The_Maze_III.py
@@ -5,8 +5,6 @@ class Solution:
     '''
     NOTE: once ball rolls over the hole, it can fall so stop BFS.
     So when rolling, need add additional condition: if (x,y) = hole, break iteration.
-
-    * Put visited.add((x,y)) right after push to heap can make running time much faster!
     '''
     def findShortestWay(self, maze: List[List[int]], ball: List[int], hole: List[int]) -> str:
         
@@ -27,27 +25,19 @@ class Solution:
 
             if not search: continue
             
-            # visited.add((x, y))
+            visited.add((x, y))
             
             x2, y, dist = self.left(hole, x, y, maze)
-            if x2 != x and (x2, y) not in visited: 
-                visited.add((x, y))
-                heapq.heappush(heap, (step+dist, (x2, y), move+"l"))
+            if x2 != x and (x2, y) not in visited: heapq.heappush(heap, (step+dist, (x2, y), move+"l"))
 
             x2, y, dist = self.right(hole, x, y, maze)
-            if x2 != x and (x2, y) not in visited:
-                visited.add((x, y))
-                heapq.heappush(heap, (step+dist, (x2, y), move+"r"))
+            if x2 != x and (x2, y) not in visited: heapq.heappush(heap, (step+dist, (x2, y), move+"r"))
                 
             x, y2, dist = self.up(hole, x, y, maze)
-            if y2 != y and (x, y2) not in visited: 
-                visited.add((x, y))
-                heapq.heappush(heap, (step+dist, (x, y2), move+"u"))
+            if y2 != y and (x, y2) not in visited: heapq.heappush(heap, (step+dist, (x, y2), move+"u"))
             
             x, y2, dist = self.down(hole, x, y, maze)
-            if y2 != y and (x, y2) not in visited: 
-                visited.add((x, y))
-                heapq.heappush(heap, (step+dist, (x, y2), move+"d"))
+            if y2 != y and (x, y2) not in visited: heapq.heappush(heap, (step+dist, (x, y2), move+"d"))
         
         ans = sorted([x[1] for x in ans if x[0] == min_step])
         
