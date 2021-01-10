cc81f0e16ca6c96a48244484636061b4c90f02ac
Triple H <Hsiang.Hung2015@gmail.com>
2021-01-10 11:11:07 -0800

    meium

--- leetcode_solution/dynamical programming/#322.Coin_Change.py
+++ leetcode_solution/dynamical programming/#322.Coin_Change.py
@@ -17,17 +17,19 @@ class Solution:
         if amount == 0: return 0        
         
         token = min(coins)
+        if token > amount: return -1
+
         dp = {x: 1 for x in coins}
-        i = min(coins)
+        i = min(coins) + 1
         while i <= amount:
             
-            if i+1 not in dp:
-                min_num = 2**31-1
-                for x in coins:
-                    if i+1-x in dp:
-                        min_num = min(min_num, dp[i+1-x]+1)
-                if min_num < 2**31-1: dp[i+1] = min_num
-                    
+            for x in coins:
+                if i - x in dp:
+                    if i not in dp:
+                        dp[i] = dp[i-x] + 1
+                    else:
+                        dp[i] = min(dp[i], dp[i-x] + 1)
+                        
             i += 1  # NOTE, the amount needs to jump increment of 1. 
                     # if coins[3,7,9] and increment = 3, we will miss anwer.
                                     
