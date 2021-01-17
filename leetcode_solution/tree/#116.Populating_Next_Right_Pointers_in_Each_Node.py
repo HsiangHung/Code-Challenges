09d33fcc4c1ff4ef6405c7f781878ca7a44405bf
Triple H <Hsiang.Hung2015@gmail.com>
2021-01-16 21:45:29 -0800

    medium

--- leetcode_solution/tree/#116.Populating_Next_Right_Pointers_in_Each_Node.py
+++ leetcode_solution/tree/#116.Populating_Next_Right_Pointers_in_Each_Node.py
@@ -1,5 +1,5 @@
-## [Leetcode#116] Populating Next Right Pointers in Each Node
-#
+#  116. Populating Next Right Pointers in Each Node (medium)
+#  https://leetcode.com/problems/populating-next-right-pointers-in-each-node/
 #
 class Solution:
     # @param root, a tree link node
@@ -9,16 +9,11 @@ class Solution:
         self.traversal(root.left, root.right)
         
     def traversal(self, left, right):
-        if not left and not right:
-            return
+        if not left or not right: return
         
         left.next = right
+
+        self.traversal(left.left, left.right)
+        self.traversal(right.left, right.right)
+        self.traversal(left.right, right.left)
         
-        if left and right:
-            self.traversal(left.left, left.right)
-            self.traversal(right.left, right.right)
-            self.traversal(left.right, right.left)
-        elif left and not right:
-            self.traversal(left.left, left.right)
-        elif not elft and right:
-            self.traversal(right.left, right.right)
