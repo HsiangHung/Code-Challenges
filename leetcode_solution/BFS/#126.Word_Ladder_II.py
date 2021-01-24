27f8046ae92877de8d37f008483006cfe8b35520
Triple H <Hsiang.Hung2015@gmail.com>
2021-01-23 21:57:04 -0800

    hard

--- leetcode_solution/BFS/#126.Word_Ladder_II.py
+++ leetcode_solution/BFS/#126.Word_Ladder_II.py
@@ -1,45 +1,48 @@
 # 126. Word Ladder II (hard)
 # https://leetcode.com/problems/word-ladder-ii/
 #
-# DFS solution works, but "Time Limit Exceeded:
-class Solution2:
+# The BFS + heap solution works, but "Time Limit Exceeded": only 21/39 test cases passed.
+class Solution:
     def findLadders(self, beginWord: str, endWord: str, wordList: List[str]) -> List[List[str]]:
         
-        self.mapping = self.get_mapping(wordList)
-        self.paths = []
-        self.min_path = None
-        self.DFS(beginWord, endWord, [beginWord], 1)
-  
-        if len(self.paths) == 0: return []
+        if endWord not in wordList: return []
         
-        return [x for x in self.paths if len(x) == self.min_path]
-    
-    
-    def DFS(self, word, endWord, path, depth):
-        if word == endWord: 
-            self.paths.append(path)
-            if self.min_path is None:
-                self.min_path = depth
-            else:
-                self.min_path = min(self.min_path, depth)
-            return
-        
-        if self.min_path is not None and depth >= self.min_path: return  # set up stop searching for shortest 
-        
-        for i in range(len(word)):
-            new_word = word[:i] + "_" + word[i+1:]
-            if new_word in self.mapping:
-                for x in self.mapping[new_word]:
-                    if x not in path:
-                        self.DFS(x, endWord, path+[x], depth + 1)
-        
-    def get_mapping(self, wordList):
-        mapping = {}
+        wordList_dict = {}
         for word in wordList:
             for i in range(len(word)):
                 new_word = word[:i]+"_"+word[i+1:]
-                if new_word not in mapping:
-                    mapping[new_word] = set({word})
+                if new_word not in wordList_dict:
+                    wordList_dict[new_word] = set([word])
                 else:
-                    mapping[new_word].add(word)
-        return mapping
+                    wordList_dict[new_word].add(word)                    
+             
+        heap = [(0, [beginWord], set([beginWord]))] # here also store set, for juding if word appeared.
+            
+        min_step = None
+        ans = []
+        while heap:
+
+            step, path, wordSet = heapq.heappop(heap)
+                  
+            if path[-1] == endWord:
+                if not min_step: 
+                    min_step = step
+                    ans.append(path)
+                elif step == min_step:
+                    ans.append(path)
+                # note, here skip if step > min_step, we only need shortest transformation
+                    
+            if min_step is not None and step >= min_step: continue
+            
+            word = path[-1]
+            for i in range(len(word)): 
+                new_word = word[:i]+"_"+word[i+1:]
+                
+                if new_word in wordList_dict:
+                    for x in wordList_dict[new_word]:
+                        if x not in path:
+                            wordSet2 = set(path+[x])
+                            heapq.heappush(heap, (step+1, path+[x], wordSet2))
+
+        return ans
+    
