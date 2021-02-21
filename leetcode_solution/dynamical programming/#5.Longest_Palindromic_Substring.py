90342c9006c3d04c588264cddc9b84641945f868
Triple H <Hsiang.Hung2015@gmail.com>
2021-02-20 18:28:27 -0800

    medium

--- leetcode_solution/dynamical programming/#5.Longest_Palindromic_Substring.py
+++ leetcode_solution/dynamical programming/#5.Longest_Palindromic_Substring.py
@@ -2,10 +2,13 @@
 #  https://leetcode.com/problems/longest-palindromic-substring/
 #
 #  Microsoft, Amazon, Bloomberg
-#
-#  use, if a Palindrome string is s[i:j], check if s[i-1] + s[i:j] + s[j] is Palindrome
 # 
 class Solution(object):
+    '''
+    if a Palindrome string is s[i:j], check if s[i-1] + s[i:j] + s[j] is Palindrome
+    each len_substring, check if dp[len_substring-2] exist. 
+    if both dp[len_substring-1] and dp[len_substring-2] not exist, no need continue search
+    '''
     def longestPalindrome(self, s):
         """
         :type s: str
@@ -15,29 +18,25 @@ class Solution(object):
         
         dp ={1: [(i,i) for i in range(len(s))], 2: [(i, i+1) for i in range(len(s)-1) if s[i] == s[i+1]]}
 
-        max_palindrome = 1
-        if len(dp[2]) > 0: max_palindrome = 2
+        max_palindrome = 2 if len(dp[2]) > 0 else 1
         
         len_substring = 3
-        noPalindrome = 0   
+        search = True   
         ## when two consective substring len shows no Palindrome, we can leave the loop.
         ## e.g. if len = 10, there is no Palindrome, then len = 12 no need to check
         ##      if len = 11, there is no Palindrome, then len = 13 no need to check
         ##      then even string has length of 100, longest palindrome is 9.
-        while len_substring <= len(s) and noPalindrome < 2:
+        while len_substring <= len(s) and search:
+            if len_substring -2 in dp:
+                dp[len_substring] = []
+                for i in range(len(dp[len_substring-2])):
+                    idx1, idx2 = dp[len_substring-2][i]
+
+                    if idx1 >= 1 and idx2 < len(s)-1 and s[idx1-1] == s[idx2+1]:
+                        dp[len_substring].append((idx1-1, idx2+1))
+                        max_palindrome = max(max_palindrome, len_substring)
             
-            dp[len_substring] = []
-            for sub_index in dp[len_substring-2]:
-                i, j = sub_index
-                if i > 0 and j < len(s)-1 and s[i-1] == s[j+1]:
-                    dp[len_substring].append((i-1, j+1))
-                                
-            if len(dp[len_substring]) > 0: 
-                max_palindrome = len_substring
-                noPalindrome = 0
-            else:
-                noPalindrome += 1
-                
+            if len_substring - 1 not in dp and len_substring-2 not in dp: search = False
             len_substring += 1
                 
         return s[dp[max_palindrome][0][0]: dp[max_palindrome][0][1]+1]
