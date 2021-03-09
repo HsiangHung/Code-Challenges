f78df06eb4a5ea262fec7d821439b3155f1a9e6b
Triple H <Hsiang.Hung2015@gmail.com>
2021-03-09 08:26:25 -0800

    easy

--- leetcode_solution/design/#706.Design_HashMap.py
+++ leetcode_solution/design/#706.Design_HashMap.py
@@ -1,4 +1,5 @@
-# #706. Design HashMap
+#  706. Design HashMap (easy)
+#  https://leetcode.com/problems/design-hashmap/
 #
 class MyHashMap:
 
@@ -6,7 +7,7 @@ class MyHashMap:
         """
         Initialize your data structure here.
         """
-        self.hash = [-1]*1000001
+        self.hash = [-1]*(1000001)
 
     def put(self, key: int, value: int) -> None:
         """
@@ -14,19 +15,20 @@ class MyHashMap:
         """
         self.hash[key] = value
 
-
     def get(self, key: int) -> int:
         """
         Returns the value to which the specified key is mapped, or -1 if this map contains no mapping for the key
         """
-        return self.hash[key]
+        if 1000000 >= key >= 0:
+            return self.hash[key]
+        else:
+            return -1
 
     def remove(self, key: int) -> None:
         """
         Removes the mapping of the specified value key if this map contains a mapping for the key
         """
         self.hash[key] = -1
-        
 
 
 # Your MyHashMap object will be instantiated and called as such:
