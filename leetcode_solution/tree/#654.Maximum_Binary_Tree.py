4b64138b7a59ef6b06c362c76d3759b8cdb4fe5c
Triple H <Hsiang.Hung2015@gmail.com>
2021-03-11 23:45:09 -0800

    medium

--- leetcode_solution/tree/#654.Maximum_Binary_Tree.py
+++ leetcode_solution/tree/#654.Maximum_Binary_Tree.py
@@ -1,39 +1,19 @@
-# [#654] Maximum Binary Tree
-#
-# 
+#  654. Maximum Binary Tree (medium)
+#  https://leetcode.com/problems/maximum-binary-tree/
 #
 class Solution(object):
-    def constructMaximumBinaryTree(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: TreeNode
-        """
-        if nums == []: return None
+    def constructMaximumBinaryTree(self, nums: List[int]) -> TreeNode:
+        if len(nums) == 0: return
+        max_val, idx = self.get_maximum(nums)
+        if max_val is not None:
+            root = TreeNode(val=max_val)
+            root.left = self.constructMaximumBinaryTree(nums[:idx])
+            root.right = self.constructMaximumBinaryTree(nums[idx+1:])
+            return root
         
-        max_val, max_idx = self.get_maximal(nums)
-        root = TreeNode(max_val)
-        
-        if len(nums) == 1: return root
-        
-        left = self.constructMaximumBinaryTree(nums[:max_idx])
-        right = self.constructMaximumBinaryTree(nums[max_idx+1:])
-        
-        root.left, root.right = left, right
-        return root
-    
-    def get_maximal(self, nums):
-        max_val, max_idx = nums[0], 0
-        for i in range(1, len(nums)):
-            if nums[i] > max_val:
-                max_val, max_idx = nums[i], i
-        return max_val, max_idx
-        prev = None
-        node = head
-        while node.next != None:
-            nextNode = node.next
-            node.next = prev
-            prev = node
-            node = nextNode
-    
-        node.next = prev
-        return node
+    def get_maximum(self, nums):
+        max_val, idx = None, None
+        for i in range(len(nums)):
+            if i == 0 or (i > 0 and nums[i] > max_val):
+                max_val, idx = nums[i], i
+        return max_val, idx
