724756d0e2ee9fc0858aaa0c9e81100749838ead
Triple H <Hsiang.Hung2015@gmail.com>
2021-03-24 22:13:13 -0700

    medium

--- leetcode_solution/DFS/#40.Combination_Sum_II.py
+++ leetcode_solution/DFS/#40.Combination_Sum_II.py
@@ -1,29 +1,31 @@
-# #40. Combination Sum II
+#  40. Combination Sum II (medium)
+#  https://leetcode.com/problems/combination-sum-ii/
 #
 class Solution:
-    def __init__(self):
-        self.sol_set = []
-        
-    def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
-        
-        for num in candidates:
-            if num > target:
-                candidates.remove(num)
-        
-        self.DFS(candidates, [], target)
-        
-        return self.sol_set
-        
-        
-    def DFS(self, candidates, sol_set, target):
-        
-        if target == 0 and len(sol_set) > 0:
-            if sorted(sol_set) not in self.sol_set:
-                self.sol_set.append(sorted(sol_set))
-            return
-        elif target < 0:
-            return
+    '''
+    the number can be repeated, but if same number, skip: i -> i+1
+    set up a sethash to avoid duplication
+    '''
+    def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:        
+        candidates = sorted(candidates)
+        return self.DFS(candidates, target)
+    
+    def DFS(self, candidates, target):
+        if len(candidates) == 0 or target <= 0: return []
+                
+        ans = []
+        i = 0
+        while i <= len(candidates)-1 and candidates[i] <= target:
+            if candidates[i] == target:
+                ans.append([candidates[i]])
+            else:
+                sol = self.DFS(candidates[i+1:], target-candidates[i])
+                for x in sol:
+                    if x[0] >= candidates[i]:
+                        ans.append([candidates[i]]+x)
+            i += 1
+            while i <= len(candidates)-1 and candidates[i] == candidates[i-1]: # trick, need to move index
+                i += 1
             
-        for i in range(len(candidates)):
-            self.DFS(candidates[i+1:], sol_set+[candidates[i]], target-candidates[i])
-            
+        ans = set([tuple(x) for x in ans])
+        return [list(x) for x in ans]
