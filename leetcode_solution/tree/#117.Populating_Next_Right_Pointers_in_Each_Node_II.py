8a70a3a919c8c7dad312234b97beb5763da98897
Triple H <Hsiang.Hung2015@gmail.com>
2021-04-25 13:12:18 -0700

    medium

--- leetcode_solution/tree/#117.Populating_Next_Right_Pointers_in_Each_Node_II.py
+++ leetcode_solution/tree/#117.Populating_Next_Right_Pointers_in_Each_Node_II.py
@@ -1,6 +1,6 @@
-## [Leetcode#117] Populating Next Right Pointers in Each Node II
-##
-##
+#  117. Populating Next Right Pointers in Each Node II (medium)
+#  https://leetcode.com/problems/populating-next-right-pointers-in-each-node-ii/
+#
 class Solution:
     # @param root, a tree link node
     # @return nothing
@@ -9,7 +9,6 @@ class Solution:
         self.level = {}
         self.traversal(0, root)
         
-    
     def traversal(self, depth, root):
         
         if depth not in self.level:
@@ -26,3 +25,30 @@ class Solution:
             
         if root.right:
             self.traversal(depth+1, root.right)
+
+
+#  BFS solution
+#
+class BFSSolution:
+    def connect(self, root: 'Node') -> 'Node':
+        if not root: return
+        
+        queue = [(root, 0)]
+        prev_node, prev_layer = None, -1
+        while queue:
+            node, layer = queue.pop(0)
+            
+            if node.left:
+                queue.append((node.left, layer + 1))
+                
+            if node.right:
+                queue.append((node.right, layer + 1))
+            
+            if layer == prev_layer: # when same layer, do .next
+                prev_node.next = node
+                
+            prev_node = node
+            prev_layer = layer
+                                       
+        return root
+            
