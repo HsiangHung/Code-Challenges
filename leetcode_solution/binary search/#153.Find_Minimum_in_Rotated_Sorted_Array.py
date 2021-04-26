f37aa388cd4cf5364264e0b7ec768f393c0b04eb
Triple H <Hsiang.Hung2015@gmail.com>
2021-04-25 19:36:13 -0700

    medium

--- leetcode_solution/binary search/#153.Find_Minimum_in_Rotated_Sorted_Array.py
+++ leetcode_solution/binary search/#153.Find_Minimum_in_Rotated_Sorted_Array.py
@@ -4,16 +4,16 @@
 class Solution:
     def findMin(self, nums: List[int]) -> int:
         
-        if len(nums) == 1: return nums[0]
-        if len(nums) == 2: return min(nums)
+        if len(nums) <= 2: return min(nums)
         
         mid = len(nums) // 2
         
-        if nums[mid+1] < nums[mid]: return nums[mid+1]
-        
-        if nums[mid] < nums[0]:
-            return self.findMin(nums[:mid+1])
-        elif nums[-1] < nums[mid]:
-            return self.findMin(nums[mid:])
-        elif nums[0] < nums[mid] < nums[-1]:
-            return nums[0]
+        if nums[mid] < nums[mid-1] and nums[mid] < nums[mid+1]:
+            return nums[mid]
+        elif nums[0] < nums[mid]:
+            if nums[mid] > nums[-1]:
+                return self.findMin(nums[mid+1:])
+            else:
+                return self.findMin(nums[:mid])
+        else:
+            return self.findMin(nums[:mid])
