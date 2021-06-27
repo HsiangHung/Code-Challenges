2ef637774a3da02aeee934382a00c03319e5ce69
Triple H <Hsiang.Hung2015@gmail.com>
2021-06-26 18:59:51 -0700

    medium

--- leetcode_solution/tree/#314.Binary_Tree_Vertical_Order_Traversal.py
+++ leetcode_solution/tree/#314.Binary_Tree_Vertical_Order_Traversal.py
@@ -1,4 +1,5 @@
-# # 314. Binary Tree Vertical Order Traversal
+#  314. Binary Tree Vertical Order Traversal
+#  https://leetcode.com/problems/binary-tree-vertical-order-traversal/
 #
 # Definition for a binary tree node.
 # class TreeNode:
@@ -8,38 +9,19 @@
 #         self.right = right
 class Solution:
     def verticalOrder(self, root: TreeNode) -> List[List[int]]:
-        '''
-        This problem is unable to solve using DFS.
-        e.g. example 3: [3,9,8,4,0,1,7,null,null,null,2,5]
-             using DFS we will obtain [[4],[9,5],[3,0,1],[2,8],[7]], 
-             not [[4],[9,5],[3,0,1],[8,2],[7]] since from left, we will
-             hit 2 first, not 8
-        
-        only using BFS we can limit from left to right, from top to bottom
-        '''
         if not root: return []
+        self.order = {}
         
-        self.col = {}
-        
-        self.BFS([(root, 0)])
-        
-        return [self.col[r] for r in sorted(self.col)]
-    
-    
-    def BFS(self, stack):
-            
-        next_stack = []
-    
-        while len(stack) > 0:
-            node, x = stack.pop(0)
-                        
-            self.col[x] = self.col.get(x, []) + [node.val]
+        queue = [(root, 0)]
+        while queue:    
+            node, x = queue.pop(0)
+            self.order[x] = self.order.get(x, []) + [node.val]
             
             if node.left:
-                next_stack.append((node.left, x-1))
+                queue.append((node.left, x-1))
                 
             if node.right:
-                next_stack.append((node.right, x+1))
+                queue.append((node.right, x+1))
+                
+        return [self.order[x] for x in sorted(self.order.keys())]
         
-        if len(next_stack) > 0:       
-            self.BFS(next_stack)
