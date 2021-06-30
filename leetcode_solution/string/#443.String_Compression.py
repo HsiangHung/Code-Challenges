ffff54f9154e89178699a78d3f4e283f929b8898
Triple H <Hsiang.Hung2015@gmail.com>
2021-06-30 00:29:29 -0700

    medium

--- leetcode_solution/string/#443.String_Compression.py
+++ leetcode_solution/string/#443.String_Compression.py
@@ -1,34 +1,36 @@
-## [Leetcode#443] String Compression
-##
-##  
+#  443. String Compression (medium)
+#  https://leetcode.com/problems/string-compression/
+#  
 class Solution(object):
-    def compress(self, chars):
-        """
-        :type chars: List[str]
-        :rtype: int
-        """
-        if len(chars) <= 1: 
-            return len(chars)
+    def compress(self, chars: List[str]) -> int:
+        '''
+        the problems requies to modify chars in-place
+        '''
+        if len(chars) == 0: return 0
         
-        count = 1
-        x = 1
-        for i in range(1, len(chars)):
-            if chars[x] == chars[x-1]:
-                count += 1 
-                del chars[x]
-            elif chars[x] != chars[x-1]:
-                if count > 1:
-                    count = str(count)
-                    for j in range(len(count)):
-                        chars.insert(x, count[j])
-                        x += 1
-                    
-                x += 1
-                count = 1
-        
-        if count > 1:
-            count = str(count)
-            for j in range(len(count)):
-                chars.append(count[j])
-                
+        ans = [chars[0]]
+        repeat = 1
+        i = 1
+        while i < len(chars):
+            if chars[i] == chars[i-1]:
+                repeat += 1
+                chars.pop(i)
+            else:
+                if repeat > 1:
+                    self.insert_nums(i, chars, repeat)
+                    i += 1
+                repeat = 1
+                i += 1
+            
+        if repeat > 1: self.insert_nums(i, chars, repeat)
+            
         return len(chars)
+    
+    def insert_nums(self, i, chars, num):
+        '''
+        e.g. 130 -> "130" -> ["1", "3", "0"]
+        '''
+        num = str(num)
+        for j in range(len(num)):
+            chars.insert(i+j, num[j])
+                    
