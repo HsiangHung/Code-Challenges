08f27d26fffe0d62b1565954f826d5d220f27c67
Triple H <Hsiang.Hung2015@gmail.com>
2021-09-26 22:13:04 -0700

    medium

--- leetcode_solution/binary search/#33.Search_in_Rotated_Sorted_Array.py
+++ leetcode_solution/binary search/#33.Search_in_Rotated_Sorted_Array.py
@@ -16,21 +16,18 @@ dclass Solution(object):
         
         mid = len(nums) // 2
         
-        a, b, c = nums[0], nums[mid], nums[-1]
-        
-        if b == target: return mid
-        if a  == target: return 0
-        if c == target: return len(nums)-1
-        
-        if b > a:       # when pivot is on the left, i.e. [1,2,4,5,7,0,1]
-            if  b > target > a:
-                return self.search(nums[:mid], target)
-            else:
-                idx = self.search(nums[mid:], target)
-                return mid + idx if idx != -1 else -1
-        elif c > b:     # when pivot is on the right, i.e. [7,0,1,2,3,4,5]
-            if c > target > b:
-                idx = self.search(nums[mid:], target)
-                return mid + idx if idx != -1 else -1
-            else:
-                return self.search(nums[:mid], target)
+        if nums[mid] == target:
+            return mid
+        else:
+            if nums[0] < nums[mid]:  # pivot is on right hand side, e.g. [2,4,5,6,7,0,1]
+                if nums[0] <= target < nums[mid]:
+                    return self.search(nums[:mid], target)
+                else:
+                    x = self.search(nums[mid+1:], target)
+                    return x + mid + 1 if x != -1 else -1
+            else:                    # pivot is on left hand side, e.g. [7,0,1,2,4,5,6]
+                if nums[mid] < target <= nums[-1]:
+                    x = self.search(nums[mid+1:], target)
+                    return x + mid + 1 if x != -1 else -1
+                else:
+                    return self.search(nums[:mid], target)
