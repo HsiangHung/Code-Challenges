d8deaa8fe540f8ffc471aca0b3b489b1ebc66701
Triple H <Hsiang.Hung2015@gmail.com>
2023-09-06 22:17:28 -0700

    array

---
+++ leetcode_solution/array/#283.Move_Zeroes.py
@@ -0,0 +1,16 @@
+#
+# 283. Move Zeroes
+#
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        
+        i = 0
+        for _ in range(len(nums)):
+            if nums[i] == 0:
+                nums.pop(i)
+                nums.append(0)
+            else:
+                i += 1
