c998d49f878dac66e9c9f15314163137f552ed5a
Triple H <Hsiang.Hung2015@gmail.com>
2026-03-07 23:05:22 -0800

    medium

--- leetcode_solution/tree/#450.Delete_Node_in_a_BST.py
+++ leetcode_solution/tree/#450.Delete_Node_in_a_BST.py
@@ -2,76 +2,64 @@
 #   
 #  Uber
 #
-class Solution(object):
-    def deleteNode(self, root, key):
-        """
-        :type root: TreeNode
-        :type key: int
-        :rtype: TreeNode
-        """
-        if not root: return root
-        
-        if root.val == key:  ## take care if the delete node is the root
-            left, right = root.left, root.right
-            if left and right:
-                min_node = self.get_min_node(right)
-                min_node.left = left
-                return right
-            elif not left:
-                return right
-            elif not right:
-                return left
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def get_min(self, parent, node):
+        if node.left:
+            return self.get_min(node, node.left)
+        # We found the successor! 
+        # Before we return it, we must detach it from its parent
+        if parent.left == node:
+            parent.left = node.right # Successor might have a right child
+        else:
+            parent.right = node.right
+            
+        return node
 
-        ## if the delete node is not the root, we need both prev and current nodes to 
-        ## perform the cut process
-        prev_delete, delete = self.binary_search(None, root, key)
+    def deleteNode(self, root: Optional[TreeNode], key: int) -> Optional[TreeNode]:
+        if not root:
+            return
 
-        if delete:
-            self.deleting(prev_delete, delete)
-            
-        return root
-       
-    def deleting(self, prev, root):
-        left, right = root.left, root.right
-        if prev.val > root.val:
-            if left and right:
-                min_node = self.get_min_node(right)
-                prev.left = right
-                min_node.left = left
-            elif not left:
-                prev.left = right
-            elif not right:
-                prev.left = left
-        elif prev.val < root.val:
-            if left and right:
-                max_node = self.get_max_node(left)
-                #print max_node.val
-                prev.right = left
-                max_node.right = right
-            elif not left:
-                prev.right = right
-            elif not right:
-                prev.right = left
-            
-    
-    def get_min_node(self, root):
-        if not root.left: return root
-        if root.left:
-            return self.get_min_node(root.left)
+        def search_node(parent, node, key):
+            """
+            binary search the node first
+            """
+            if not node:
+                return None, None
+            if node.val == key:
+                return parent, node
+            elif node.val > key:
+                return search_node(node, node.left, key)
+            elif node.val < key:
+                return search_node(node, node.right, key)
+
+        parent, node = search_node(None, root, key)
+        if node is None: # if didn't find the node == key
+            return root
+
+        if node.left and node.right: 
+            # if node exists both left and right, replaced by min of right branch
+            new_node = self.get_min(node, node.right)
+            new_node.left = node.left
+            new_node.right = node.right
+        elif node.left and not node.right:
+            # if node exists left only, replaced by node.left
+            new_node = node.left
+        elif not node.left and node.right:
+            # if node exists right only, replaced by node.right
+            new_node = node.right
+        else:
+            new_node = None
+        if node == root:
+            return new_node
         
-    def get_max_node(self, root):
-        if not root.right: return root
-        if root.right:
-            return self.get_max_node(root.right)
-    
-    def binary_search(self, prev, root, key):
-        if root.val == key: return prev, root
+        if node == parent.right :
+            parent.right = new_node
+        else:
+            parent.left = new_node
         
-        if not root.left and not root.right: return None, None
-        
-        if root.val > key:
-            return self.binary_search(root, root.left, key)
-        elif root.val < key:
-            return self.binary_search(root, root.right, key)
-            
-        
+        return root 
