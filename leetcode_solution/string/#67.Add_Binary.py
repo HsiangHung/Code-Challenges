48758661feda125307a06158988e77fe257318ea
Triple H <Hsiang.Hung2015@gmail.com>
2026-05-08 23:12:36 -0700

    add binary

--- leetcode_solution/string/#67.Add_Binary.py
+++ leetcode_solution/string/#67.Add_Binary.py
@@ -5,29 +5,34 @@
 class Solution(object):
     def addBinary(self, a, b):
         """
-        :type a: str
-        :type b: str
-        :rtype: str
-        """
-        a = [int(s) for s in a][::-1]
-        b = [int(s) for s in b][::-1]
+          1111
+        +   11
+         -----
+         10010  
 
-        if len(a) >= len(b):
-            short, long = b, a
-        else:
-            short, long = a, b
-                    
-        for i in range(len(short)):
-            long[i] += short[i]
+         x+y = 3 => 11
+         x+y = 2 => 10
+         x+y = 1 => 01
+         x+y = 0 => 00
+        """
+
+        a_,  b_ = list(a)[::-1], list(b)[::-1]
+
+        res = []
+        i, j = 0, 0
+        next_digit = 0
+        while i < len(a) or j < len(b):
+            x, y = a_[i] if i < len(a) else 0, b_[j] if j < len(b) else 0
+
+            binary_sum = int(x) + int(y) + next_digit
+            
+            res.append(binary_sum % 2)
+            next_digit = binary_sum // 2
+
+            i += 1
+            j += 1
         
-        for i in range(len(long)-1):
-            if long[i] >= 2:
-                long[i] -= 2
-                long[i+1] += 1
-        
-        if long[-1] >= 2:
-            long[-1] -= 2
-            long.append(1)
-                    
-        return "".join([str(x) for x in long[::-1]])
-                    
+        if next_digit == 1:
+            res.append(1)
+
+        return "".join([str(x) for x in res[::-1]])
