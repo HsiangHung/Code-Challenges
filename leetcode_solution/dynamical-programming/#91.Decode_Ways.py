9a7d39e7c0881f9aba9926d162072db38dbb8426
Hsiang <Hsiang.Hung2015@gmail.com>
2026-06-11 16:58:07 -0700

    new version #91

--- leetcode_solution/dynamical-programming/#91.Decode_Ways.py
+++ leetcode_solution/dynamical-programming/#91.Decode_Ways.py
@@ -34,9 +34,40 @@ class Solution:
         i = 2
         while i <= len(s):
             dp[i] = 0
-            if  s[i-2: i] <= "26" and s[i-2] != "0": dp[i] = dp[i-2]
-            if s[i-1] != "0": dp[i] += dp[i-1]
+            if s[i-2: i] <= "26" and s[i-2] != "0": # consider s[i-2] = "1", s[i-1] = "0", then "10" is valid
+                dp[i] = dp[i-2]
+            if s[i-1] != "0": # add curr s[i]
+                dp[i] += dp[i-1]
             i += 1
         
         return dp[len(s)]
+    
+
+     def dp_list_all_decodes(self, s: str):
+        """
+        This version lists all decoding ways, and hits memory limitation.
+        But code is clear.
+        """
         
+        alpha_map = {"1": "A", "2": "B", "3": "C", "4": "D", "5": "E", 
+                     "6": "F", "7": "G", "8": "H", "9": "I", "10": "J", 
+                     "11": "K", "12": "L", "13": "M", "14": "N", "15": "O", "16": "P", "17": "Q", "18": "R", "19": "S", "20": "T",
+                     "21": "U", "22": "V", "23": "W", "24": "X", "25": "Y",
+                     "26": "Z"}
+
+        dp = {0: [[s[0]]]}
+        for i in range(1, len(s)):
+            prev = dp[i-1]
+
+            curr = []
+            for j in prev:
+
+                if s[i] != "0":
+                    curr.append( j + [s[i]])
+
+                if len(j[-1]) < 2 and (j[-1] + s[i] in alpha_map):
+                    curr.append(j[:-1] + [j[-1] + s[i]])
+
+            dp[i] = curr
+
+        return len(dp[len(s)-1])
