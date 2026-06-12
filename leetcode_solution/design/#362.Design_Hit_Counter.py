dacb448d13a65610a49c169a191313860909f2b9
Hsiang <Hsiang.Hung2015@gmail.com>
2026-06-11 20:29:55 -0700

    short version on #362

--- leetcode_solution/design/#362.Design_Hit_Counter.py
+++ leetcode_solution/design/#362.Design_Hit_Counter.py
@@ -4,44 +4,18 @@
 class HitCounter:
 
     def __init__(self):
-        """
-        Initialize your data structure here.
-        """
-        self.data = {}
-        self.window_end = 300
-
+        self.ts_queue = []
+        
     def hit(self, timestamp: int) -> None:
-        """
-        Record a hit.
-        @param timestamp - The current timestamp (in seconds granularity).
-        """
-        self.data[timestamp] = self.data.get(timestamp, 0) + 1
-        
-        if timestamp > self.window_end:
-            self.window_end = timestamp
-            times = list(self.data.keys()) # if use times = self.data.keys(), it changes dict size
-            
-            for t in times:
-                if t < timestamp - 300 + 1:
-                    del self.data[t]
-                    
+        self.ts_queue.append(timestamp)
+        while timestamp - self.ts_queue[0] >= 300:
+            self.ts_queue.pop(0)
+
     def getHits(self, timestamp: int) -> int:
-        """
-        Return the number of hits in the past 5 minutes.
-        @param timestamp - The current timestamp (in seconds granularity).
-        """
-        
-        if timestamp > self.window_end:
-            
-            self.window_end = timestamp
-            times = list(self.data.keys())  # if use times = self.data.keys(), it changes dict size
-            
-            for t in times:
-                if t < timestamp - 300 + 1:
-                    del self.data[t]
-        
-        return sum([self.data[t] for t in self.data])
-        
+        while len(self.ts_queue) > 0 and timestamp - self.ts_queue[0] >= 300:
+            self.ts_queue.pop(0)
+
+        return len(self.ts_queue)
 
 
 # Your HitCounter object will be instantiated and called as such:
