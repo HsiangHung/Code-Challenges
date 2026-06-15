f3b78e14cc11575a3c78bfd2d95929af2407f01a
Hsiang <Hsiang.Hung2015@gmail.com>
2026-06-15 15:08:37 -0700

    binary search #374

--- leetcode_solution/README.md
+++ leetcode_solution/README.md
@@ -166,6 +166,7 @@
 | 364 | Nested List Weight Sum II | DFS | medium | [link](https://github.com/HsiangHung/Code-Challenges/blob/master/leetcode_solution/DFS/%23364.Nested_List_Weight_Sum_II.py) |
 | 366 | Find Leaves of Binary Tree | tree | medium | [link](https://github.com/HsiangHung/Code-Challenges/blob/master/leetcode_solution/tree/%23366.Find_Leaves_of_Binary_Tree.py) |
 | 369 | Plus One Linked List | linked list | medium | [link](https://github.com/HsiangHung/Code-Challenges/blob/master/leetcode_solution/linked-list/%23369.Plus_One_Linked_List.py)|
+| 374 | Guess Number Higher or Lower | binary search | easy | [link](https://github.com/HsiangHung/Code-Challenges/blob/master/leetcode_solution/binary-search/%23374.Guess_Number_Higher_or_Lower.py)|
 | 377 | Combination Sum IV | dynamical programming | medium |[link](https://github.com/HsiangHung/Code-Challenges/blob/master/leetcode_solution/dynamical-programming/%23377.Combination_Sum_IV.py) |
 | 378 | Kth Smallest Element in a Sorted Matrix | heap | medium | [link](https://github.com/HsiangHung/Code-Challenges/blob/master/leetcode_solution/heap/%23378.Kth_Smallest_Element_in_Sorted_Matrix.py) |
 | 380 | Insert Delete GetRandom O(1) | design | medium | [link](https://github.com/HsiangHung/Code-Challenges/blob/master/leetcode_solution/design/%23380.Insert_Delete_GetRandom_O(1).py) |
--- leetcode_solution/binary-search/#374.Guess_Number_Higher_or_Lower.py
+++ leetcode_solution/binary-search/#374.Guess_Number_Higher_or_Lower.py
@@ -1,29 +1,34 @@
-# [#374] Guess Number Higher or Lower
 #
-#   google
+#  374. Guess Number Higher or Lower
 #
 # The guess API is already defined for you.
 # @param num, your guess
-# @return -1 if my number is lower, 1 if my number is higher, otherwise return 0
-# def guess(num):
+# @return -1 if num is higher than the picked number
+#          1 if num is lower than the picked number
+#          otherwise return 0
+# def guess(num: int) -> int:
 
-class Solution(object):
-    def guessNumber(self, n):
-        """
-        :type n: int
-        :rtype: int
-        """
-        if n == 1: return n
-        
+class Solution:
+    def guessNumber(self, n: int) -> int:
+
+        if n == 1:
+            return n
+
         start, end = 1, n
-        while end - start != 0:
-            mid = (end - start + 1)//2 + start
-            ans = guess(mid)
-            if ans == 0:
-                return mid
-            elif ans == -1:
-                end = mid - 1
-            elif ans == 1:
-                start = mid + 1
+        while end - start > 0:
 
-        return end
+            if guess(end) == 0:
+                return end
+
+            if guess(start) == 0:
+                return start
+
+            mid = (end - start) // 2 + start
+            resp = guess(mid)
+            if resp == 0:
+                return mid
+            elif resp == -1:
+                end = mid
+            else:
+                start = mid
+        
