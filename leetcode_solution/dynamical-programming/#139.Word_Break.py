6e4f3ebcc895a1a0e1708a855915768a3a5c4181
Hsiang <Hsiang.Hung2015@gmail.com>
2026-06-14 17:51:24 -0700

    add comments in #139

--- leetcode_solution/dynamical-programming/#139.Word_Break.py
+++ leetcode_solution/dynamical-programming/#139.Word_Break.py
@@ -3,21 +3,36 @@
 #
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
-        '''
+        """
         http://bangbingsyb.blogspot.com/2014/11/leetcode-word-break-i-ii.html
         https://baihuqian.github.io/2018-06-24-word-break/
         if s is breakable, s[k:] is in dictionary and s[j:k] is in dict .... and s[0:j] is in
-        '''
-        wordDict = set(wordDict)
-                
-        dp = [False]*(len(s)+1)
+
+        e.g.
+        s = "leetcode",  wordDict = ["leet", "code"]
+        dp[0] = True   (base case: empty string)
+        dp[1] = False  (no word ends at index 1)
+        dp[2] = False
+        dp[3] = False
+        dp[4] = True   ← dp[0]=T and s[0:4]="leet" ∈ dict
+        dp[5] = False
+        dp[6] = False
+        dp[7] = False
+        dp[8] = True   ← dp[4]=T and s[4:8]="code" ∈ dict  ✓
+        """
+
+        word_dict = set(wordDict)
+
+        dp = [False] * (len(s) + 1) 
         dp[0] = True
-        for i in range(len(s)):
-            for j in range(i+1, len(s)+1):
-                if s[i:j] in wordDict and dp[i]:
+
+        for i in range(len(s) + 1):
+            for j in range(i+1, len(s) + 1):
+                if s[i:j] in word_dict and dp[i] is True:
+                    print(i, j, s[i:j])
                     dp[j] = True
-                        
-        return dp[-1]
+        
+        return dp[len(s)]
 
 
 ## NOTE: using the following BFS solution cannot pass all test cases. Time exceed. 
