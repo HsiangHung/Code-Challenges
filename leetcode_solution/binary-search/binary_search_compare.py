772f65ec6d481d949a5d6608b38a9caa270a466a
Hsiang <Hsiang.Hung2015@gmail.com>
2026-06-22 08:12:23 -0700

    binary search comparison

---
+++ leetcode_solution/binary-search/binary_search_compare.py
@@ -0,0 +1,48 @@
+# Binary search for arr[i] < target but arr[i+1] >= target:
+
+
+# Option 1: recrusion
+# slow. When running recusion, sliding arr[:mid] or arr[mid:] takes time complexity O(n)
+
+def search1(target, arr):
+    """
+    This func looks for index where arr[i] < target but arr[i+1] >= target
+    """
+    if arr[0] >= target:
+        return 0
+    elif arr[-1] < target:
+        return -1 
+    
+    mid = len(arr) // 2
+    if arr[mid-1] < target and arr[mid] >= target:
+        return mid
+    elif arr[mid] < target:
+        res = search1(target, arr[mid:])
+        return res + mid if res != -1 else -1
+    else:
+        res = search1(target, arr[:mid])
+        return res if res != -1 else -1
+
+
+
+# Option 2: Iterative
+# fast. Using two pointer l, r to graudally approach i
+
+def search2(target, arr):
+    """
+    This func looks for index where arr[i] < target but arr[i+1] >= target
+    """
+    if arr[0] >= target:
+        return 0
+    elif arr[-1] < target:
+        return -1 
+
+    l, r = 0, len(arr)-1
+
+    # there must be a soution found; otherwise at the beginning it will return
+    while l <= r:
+        mid = (r + l) // 2
+        if arr[mid-1] < target and arr[mid] >= target:
+            return mid
+        elif arr[mid] < target:
+            l = mid + 1
