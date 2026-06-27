07621ee84715a96a8700173934047a5271a0aed5
Hsiang <Hsiang.Hung2015@gmail.com>
2026-06-26 23:55:19 -0700

    add #496

--- leetcode_solution/stack/#496.Next_Greater_Element_I.py
+++ leetcode_solution/stack/#496.Next_Greater_Element_I.py
@@ -3,7 +3,10 @@
 #
 class Solution:
     def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        
+        """
+        stack solution: https://www.youtube.com/watch?v=sDKpIO2HGq0
+        But here I simply use dict to solve
+        """
         nums2_idx = {}
         for i in range(len(nums2)):
             nums2_idx[nums2[i]] = nums2_idx.get(nums2[i], []) + [i]
