316e3f098e23507f22c4654dad72361b58ee4301
Hsiang <Hsiang.Hung2015@gmail.com>
2026-06-27 13:44:00 -0700

    revise #54

--- leetcode_solution/array/#54.Spiral_Matrix.py
+++ leetcode_solution/array/#54.Spiral_Matrix.py
@@ -1,35 +1,34 @@
-#  54. Spiral Matrix (medium)
-#  https://leetcode.com/problems/spiral-matrix/
+#
+# 54. Spiral Matrix
 #
 class Solution:
-    '''
-    4-directions to rotate. only hit boundary or visited (x,y), make turn
-    '''
     def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
-                
-        if len(matrix) == 0 or len(matrix[0]) == 0: return []
+        """
+        path move:
+        * 3x3, path [2,2,2,1,1] -> [2,2,1,1]
+        * 4x4, path [3,3,3,2,2,1,1] -> [3,3,2,2,1,1]
+        * 3x4, path [3,2,3,1,2] -> [2,3,1,2]
+
+        excpet first one, start from (n-1), (m-1) and decay -1 every two
+        """
+        res = matrix[0]
+
+        ny, nx = len(matrix), len(matrix[0])
+
+        directions = [(0, 1), (-1, 0), (0, -1), (1, 0)]
+        direct = 0
         
-        directions = [(1, 0), (0, 1), (-1, 0), (0, -1)]
-        ans = [matrix[0][0]]
-        
-        x, y = 0, 0
-        
-        i = 0
-        dx, dy = directions[i]
-        visited = set({(x, y)})
-        for _ in range(len(matrix)*len(matrix[0])):
-            
-            if len(visited) == len(matrix)*len(matrix[0]):
-                return ans
-            
-            x2, y2 = x+dx, y+dy
-            
-            if (not len(matrix[0]) > x2 >= 0) or (not len(matrix) > y2 >= 0) or (x2, y2) in visited:
-                i += 1
-                dx, dy = directions[i % 4]
-                x2, y2 = x+dx, y+dy
-            
-            ans.append(matrix[y2][x2])
-            visited.add((x2, y2))
-            
-            x, y = x2, y2
+        x, y = nx - 1, 0
+        steps = [ny - 1, nx - 1] 
+        while steps[direct % 2] > 0:
+
+            dx, dy = directions[direct % 4]
+            for _ in range(steps[direct % 2]):
+                x += dx
+                y += dy
+                res.append(matrix[y][x])
+
+            steps[direct % 2] -= 1 
+            direct += 1
+
+        return res
