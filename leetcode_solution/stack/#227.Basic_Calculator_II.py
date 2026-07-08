6a6584b385dda7c3ae986e6d7d235563a076a645
Hsiang <Hsiang.Hung2015@gmail.com>
2026-07-08 13:36:10 -0700

    revise #227

--- leetcode_solution/stack/#227.Basic_Calculator_II.py
+++ leetcode_solution/stack/#227.Basic_Calculator_II.py
@@ -3,66 +3,48 @@
 #
 class Solution:
     def calculate(self, s: str) -> int:
-        class Solution:
-    def calculate(self, s: str) -> int:
         """
-        This code has three parts:
-        1. first deal with numbers > 10, need to concat num char. "14" != ["1", "4"]
-        2. deal with "*" and "/" operations.
-        3. deal with "+" and "-" operations and get answer
+        ref: https://www.youtube.com/watch?v=G2AZJDkh6_E
+        e.g. s = "33 + 6*4/2-5"
+        s = ["3", "3", "+", "6", "*", "4", "/", "2", "-", "5"] + ["+"]
+
+         ch  num   last_op        stack
+              ""      "+"         []
+        "3"   "3"     "+"         []
+        "3"  "33"     "+"         []
+        "+"   ""      "+"         [33]
+        "6"   "6"     "+"         [33]
+        "*"   ""      "+" -> "*"  [33,6]
+        "4"   "4"     "*"         [33,6] 
+        "/"   ""      "*" -> "/"  [33,6] -> [33], 6*4=24 -> [33,24] 
+        "2"   "2"     "/"         [33,24] 
+        "-"   ""      "/" -> "-"  [33,24] -> [33], 24/2 -> [33,12]    
+        "5"   "5"     "-"         [33,12]
+        "+"   ""      "-" -> "+"  [33,12] + [-5] 
+                                  [33,12,-5]
         """
+        s = [x for x in list(s) if x != " "]
+        s.append("+") # IMPORTANT! forces the last number to flush at the end
 
-        num_set = set([str(x) for x in range(10)])
-
-        # 1. deal with numbers > 10, e.g. "140/30 + 2" -> ["140", "/", "30", "+", "2"]
         stack = []
-        i = 0
-        while i < len(s):
-            char = s[i]
-            if char != " ":
-                if char not in num_set:
-                    stack.append(char) # "+", "-", "*", "/" operations
-                else:
-                    j = i + 1
-                    while j < len(s) and s[j] in num_set:
-                        char += s[j] 
-                        j += 1
-                    stack.append(char)
-                    i = j - 1
-            i += 1
-    
-        # 2. deal with "*" and "/" first. e.g. ["14","/","3","-","2"] -> ["4", "-", "2"]
-        i = 0
-        while i < len(stack):
-            # print(i, stack[i])
-            if stack[i] in ("*", "/"):
-                op = stack.pop(i)
-                num2 = int(stack.pop(i))
-                num1 = int(stack.pop(i-1))
-                if op == "*":
-                    stack.insert(i-1, str(num1 * num2))
-                else: # if op = "/"
-                    stack.insert(i-1, str(int(num1 / num2)))
-            else:
-                i += 1
+        num = ""
+        last_op = "+"
+        for ch in s:
+            if ch not in ("+", "-", "*", "/"): # continguous number, e.g. "123"
+                num += ch
+            else:  # now, be "+", "-", "*", "/"
 
-        
-        # 3. deal with "+" and "-", e.g. ["3", "+", "2"] => 3 + 2
-        i, res = len(stack)-1, 0
-        prev_ch = ""
-        while i >= 0:
-            # print(i, stack[i], res)
-            if stack[i] not in ("+", "-"):
-                char = str(stack[i]) + prev_ch
-                prev_ch = char
-            else:
-                op = stack[i]
-                if op == "+":
-                    res += int(char)
+                n = int(num)
+                if last_op == "+":
+                    stack.append(n)
+                elif last_op == "-":
+                    stack.append(-n)
+                elif last_op == "*":
+                    stack.append(stack.pop()*n)
                 else:
-                    res -= int(char)
-                prev_ch = ""
-    
-            i -= 1
-
-        return res + int(char)
+                    stack.append(int(stack.pop()/n))
+                
+                last_op = ch
+                num = ""
+            
+        return sum(stack)
