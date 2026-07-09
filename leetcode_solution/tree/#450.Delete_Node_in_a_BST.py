1fa2aeda349c1a7a61c726b749d3ee24d400eaaf
Hsiang <Hsiang.Hung2015@gmail.com>
2026-07-09 15:58:18 -0700

    update

--- leetcode_solution/tree/#450.Delete_Node_in_a_BST.py
+++ leetcode_solution/tree/#450.Delete_Node_in_a_BST.py
@@ -8,58 +8,34 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def get_min(self, parent, node):
-        if node.left:
-            return self.get_min(node, node.left)
-        # We found the successor! 
-        # Before we return it, we must detach it from its parent
-        if parent.left == node:
-            parent.left = node.right # Successor might have a right child
-        else:
-            parent.right = node.right
-            
-        return node
-
     def deleteNode(self, root: Optional[TreeNode], key: int) -> Optional[TreeNode]:
+        """
+        ref: https://www.youtube.com/watch?v=LFzAoJJt92M&t=75s
+        part of code is optimized by Claude
+        """
         if not root:
-            return
+            return None
 
-        def search_node(parent, node, key):
-            """
-            binary search the node first
-            """
-            if not node:
-                return None, None
-            if node.val == key:
-                return parent, node
-            elif node.val > key:
-                return search_node(node, node.left, key)
-            elif node.val < key:
-                return search_node(node, node.right, key)
+        def get_min_node(node):
+            if not node.left:
+                return node
+            return get_min_node(node.left)
 
-        parent, node = search_node(None, root, key)
-        if node is None: # if didn't find the node == key
+        if root.val == key:
+            if not root.left and not root.right:
+                return None
+            elif not root.left and root.right:
+                return root.right
+            elif root.left and not root.right:
+                return root.left
+            else:
+                successor = get_min_node(root.right)
+                root.val = successor.val
+                root.right = self.deleteNode(root.right, successor.val)
+                return root
+        elif root.val > key:
+            root.left = self.deleteNode(root.left, key)
             return root
-
-        if node.left and node.right: 
-            # if node exists both left and right, replaced by min of right branch
-            new_node = self.get_min(node, node.right)
-            new_node.left = node.left
-            new_node.right = node.right
-        elif node.left and not node.right:
-            # if node exists left only, replaced by node.left
-            new_node = node.left
-        elif not node.left and node.right:
-            # if node exists right only, replaced by node.right
-            new_node = node.right
         else:
-            new_node = None
-        if node == root:
-            return new_node
-        
-        if node == parent.right :
-            parent.right = new_node
-        else:
-            parent.left = new_node
-        
-        return root 
+            root.right = self.deleteNode(root.right, key)
+            return root
