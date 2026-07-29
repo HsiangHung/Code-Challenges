f8432ed2bb966285a759e2cdc74fc5affb4d3feb
Hsiang <Hsiang.Hung2015@gmail.com>
2026-07-28 20:11:55 -0700

    update #606

--- leetcode_solution/tree/#606.Construct_String_from_Binary_Tree.py
+++ leetcode_solution/tree/#606.Construct_String_from_Binary_Tree.py
@@ -1,27 +1,21 @@
 #[Leetcode#606] Construct String from Binary Tree
 #
-class Solution(object):
-    def tree2str(self, t):
-        """
-        :type t: TreeNode
-        :rtype: str
-        """
-        if not t: return ""
-        
-        return self.traverse(t)
-    
-    
-    def traverse(self, root):
-        if not root.left and not root.right:
-            return str(root.val)
-        
-        tree_string = "("
-        if root.left:
-            tree_string += self.traverse(root.left)
-            
-        tree_string += ")"
-        
-        if root.right:
-            tree_string += "("+self.traverse(root.right)+")"
-        
-        return str(root.val)+tree_string
+class Solution:
+    def tree2str(self, root: Optional[TreeNode]) -> str:
+
+        def dfs(node):
+            if not node.left and not node.right:
+                return f"({node.val}"
+
+            res = f"({node.val}"
+            if node.left:
+                res += f"{dfs(node.left)})"
+            else:
+                res += f"()"
+
+            if node.right:
+                res += f"{dfs(node.right)})"
+
+            return res
+
+        return dfs(root)[1:] # root val doesn't need "(" and ")" in the end
